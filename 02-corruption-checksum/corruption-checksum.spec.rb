43a63bd1b29a8488dcbd6c4ac601b1b9379d4522
PakkuDon <pakku.pd@gmail.com>
2017-12-25 23:59:37 +1100

    Add solution for Day 2, Part 1 - Corruption Checksum

---
+++ 02-corruption-checksum/corruption-checksum.spec.rb
@@ -0,0 +1,15 @@
+require "./corruption-checksum"
+
+describe "corruption-checksum" do
+  it "calculates checksum as sum of differences between min and max values per row" do
+    data = [
+      [5, 1, 9, 5],
+      [7, 5, 3],
+      [2, 4, 6, 8],
+    ]
+    expected_sum = 18
+
+    actual_sum = CorruptionChecksum.call(data)
+    expect(expected_sum).to eq(actual_sum)
+  end
+end
---
+++ 02-corruption-checksum/index.rb
@@ -0,0 +1,6 @@
+require "./corruption-checksum"
+
+puzzle_input = File.read("input.txt")
+spreadsheet = puzzle_input.split("\n").map { |row| row.split(/\s+/).map(&:to_i) }
+
+puts CorruptionChecksum.call(spreadsheet)
