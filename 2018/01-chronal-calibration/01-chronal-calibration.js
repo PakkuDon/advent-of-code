59bcc69d364fb638695d00c72a3cc686c2639981
PakkuDon <pakku.pd@gmail.com>
2018-12-01 16:10:12 +1100

    Add solution for 2018, Day 1, Part 1 - Chronal Calibration

---
+++ 2018/01-chronal-calibration/01-chronal-calibration.js
@@ -0,0 +1,3 @@
+const chronalCalibration = (frequencies) => frequencies.reduce((a, b) => a + b, 0)
+
+module.exports = chronalCalibration
---
+++ 2018/01-chronal-calibration/01-chronal-calibration.test.js
@@ -0,0 +1,18 @@
+const chronalCalibration = require('./01-chronal-calibration')
+
+describe('Chronal Calibration', () => {
+  test('+1, +1, +1 returns 3', () => {
+    const input = [1, 1, 1]
+    expect(chronalCalibration(input)).toBe(3)
+  })
+
+  test('+1, +1, -2 returns 0', () => {
+    const input = [+1, +1, -2]
+    expect(chronalCalibration(input)).toBe(0)
+  })
+
+  test('-1, -2, -3 returns -6', () => {
+    const input = [-1, -2, -3]
+    expect(chronalCalibration(input)).toBe(-6)
+  })
+})
