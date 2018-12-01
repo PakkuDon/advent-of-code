c292e0f11c362da058a05e97db7fba8969e11ff9
PakkuDon <pakku.pd@gmail.com>
2018-12-01 18:20:04 +1100

    Add solution for 2018, Day 1, Part 2 - Chronal Calibration

---
+++ 2018/01-chronal-calibration/02-chronal-calibration.js
@@ -0,0 +1,19 @@
+const chronalCalibration = (frequencies) => {
+  const pastFrequencies = []
+  let currentFrequency = 0
+
+  pastFrequencies.push(currentFrequency)
+
+  while (true) {
+    for (let i = 0; i < frequencies.length; i++) {
+      currentFrequency += frequencies[i]
+      pastFrequencies.push(currentFrequency)
+
+      if (pastFrequencies.indexOf(currentFrequency) !== pastFrequencies.lastIndexOf(currentFrequency)) {
+        return currentFrequency
+      }
+    }
+  }
+}
+
+module.exports = chronalCalibration
---
+++ 2018/01-chronal-calibration/02-index.js
@@ -0,0 +1,7 @@
+const fs = require('fs')
+const chronalCalibration = require('./02-chronal-calibration')
+
+const puzzleInput = fs.readFileSync('./input.txt', 'utf8').trim()
+const frequencies = puzzleInput.split(/\s/).map(value => parseInt(value))
+
+console.log(chronalCalibration(frequencies))
