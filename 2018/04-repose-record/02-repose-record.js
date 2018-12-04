fddd410cd21786f5e217e87cf47675f8a3baa27b
PakkuDon <pakku.pd@gmail.com>
2018-12-04 17:53:27 +1100

    Add solution for 2018, Day 4, Part 2 - Prose Record

---
+++ 2018/04-repose-record/02-repose-record.js
@@ -0,0 +1,85 @@
+const checkSum = (records) => {
+  const events = parseRecords(records)
+
+  const minutesAsleepByGuard = {}
+  events.forEach((event, index) => {
+    if (!minutesAsleepByGuard[event.guardId]) {
+      minutesAsleepByGuard[event.guardId] = []
+    }
+
+    const previousEvent = events[index - 1]
+    if (previousEvent && !previousEvent.awake) {
+      for (let i = previousEvent.minute; i < event.minute; i++) {
+        minutesAsleepByGuard[event.guardId].push(i)
+      }
+    }
+  })
+
+  const tallySleepByGuardByMinutes = {}
+  for (let guardId in minutesAsleepByGuard) {
+    tallySleepByGuardByMinutes[guardId] = {}
+    minutesAsleepByGuard[guardId].forEach((minute) => {
+      if (!tallySleepByGuardByMinutes[guardId][minute]) {
+        tallySleepByGuardByMinutes[guardId][minute] = 0
+      }
+      tallySleepByGuardByMinutes[guardId][minute]++
+    })
+  }
+
+  let mostTimesAsleepOnSameMinute = 0
+  let minuteWithRecord
+  let guardWithRecord
+  for (let guardId in tallySleepByGuardByMinutes) {
+    for (let minute in tallySleepByGuardByMinutes[guardId]) {
+      const amountOfTimesAsleep = tallySleepByGuardByMinutes[guardId][minute]
+      if (amountOfTimesAsleep > mostTimesAsleepOnSameMinute) {
+        mostTimesAsleepOnSameMinute = amountOfTimesAsleep
+        minuteWithRecord = minute
+        guardWithRecord = guardId
+      }
+    }
+  }
+
+  return minuteWithRecord * guardWithRecord
+}
+
+const parseRecords = (records) => {
+  records.sort()
+
+  let events = records.sort().map((record, index, array) => {
+    let guardId
+    let awake
+    const [ _, day, hour, minute ] = record.match(/\[\d{4}-\d{2}-(\d{2}) (\d{2}):(\d{2})/)
+
+    if (record.includes('begins shift')) {
+      guardId = record.match(/#(\d+)/)[1]
+      awake = true
+    }
+    if (record.includes('falls asleep')) {
+      awake = false
+    }
+    else if (record.includes('wakes up')) {
+      awake = true
+    }
+
+    return {
+      guardId: parseInt(guardId),
+      hour: parseInt(hour),
+      minute: parseInt(minute),
+      day: parseInt(day),
+      awake,
+    }
+  })
+
+  events.forEach((event, index) => {
+    if (!event.guardId) {
+      event.guardId = events[index - 1].guardId
+    }
+  })
+
+  return events
+}
+
+module.exports = {
+  checkSum,
+}
--- 2018/04-repose-record/index.js
+++ 2018/04-repose-record/index.js
@@ -1,5 +1,9 @@
 const fs = require('fs')
 const proseRecordPart1 = require('./01-repose-record')
+const proseRecordPart2 = require('./02-repose-record')
 
 const puzzleInput = fs.readFileSync('./input.txt', 'utf8').trim().split('\n')
+console.log('Part 1')
 console.log(proseRecordPart1.checkSum(puzzleInput))
+console.log('Part 2')
+console.log(proseRecordPart2.checkSum(puzzleInput))
