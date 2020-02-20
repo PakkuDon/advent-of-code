1e82fb35b28400e889779f25bb2f4682ba0a2d7a
PakkuDon <pakku.pd@gmail.com>
2020-02-20 23:59:47 +1100

    Add solution for 2019, Day 1, Part 2

---
+++ 2019/01-the-tyranny-of-the-rocket-equation/02.js
@@ -0,0 +1,18 @@
+const calculateFuelRequired = require('./01')
+
+const calculateFuelRequiredWithFuel = mass => {
+  let fuelRequired = calculateFuelRequired(mass)
+  let fuelRemaining = fuelRequired
+
+  while (fuelRemaining > 0) {
+    fuelRemaining = calculateFuelRequired(fuelRemaining)
+
+    if (fuelRemaining > 0) {
+      fuelRequired += fuelRemaining
+    }
+  }
+
+  return fuelRequired
+}
+
+module.exports = calculateFuelRequiredWithFuel
--- 2019/01-the-tyranny-of-the-rocket-equation/index.js
+++ 2019/01-the-tyranny-of-the-rocket-equation/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const calculateFuelRequired = require('./01')
+const calculateFuelRequiredWithFuel = require('./02')
 
 const puzzleInput = fs.readFileSync('./input.txt', 'utf8')
 const moduleMasses = puzzleInput
@@ -11,5 +12,10 @@ const sumOfFuelRequirements = moduleMasses
   .map(mass => calculateFuelRequired(mass))
   .reduce((total, currentMass) => total + currentMass, 0)
 
+const sumOfFuelRequirementsIncludingFuel = moduleMasses
+  .map(mass => calculateFuelRequiredWithFuel(mass))
+  .reduce((total, currentMass) => total + currentMass, 0)
+
 console.log(`Part 1: ${sumOfFuelRequirements}`)
+console.log(`Part 2: ${sumOfFuelRequirementsIncludingFuel}`)
 
