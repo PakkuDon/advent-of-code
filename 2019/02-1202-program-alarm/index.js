abcce30b5f4ffc6ecbea7780db66a114904e632e
PakkuDon <pakku.pd@gmail.com>
2020-02-22 23:38:30 +1100

    Add solution for 2019, Day 2, Part 2

--- 2019/02-1202-program-alarm/index.js
+++ 2019/02-1202-program-alarm/index.js
@@ -12,6 +12,27 @@ const lastObservedOpsCode = inputOpsCode.slice()
 lastObservedOpsCode[1] = 12
 lastObservedOpsCode[2] = 2
 
-const parsedOpsCode = parser(lastObservedOpsCode)
+let parsedOpsCode = parser(lastObservedOpsCode)
 
 console.log(`Part 1: ${parsedOpsCode[0]}`)
+
+let solutionFound = false
+let i, j
+for (i = 0; i < 100; i++) {
+  for (j = 0; j < 100; j++) {
+    lastObservedOpsCode[1] = i
+    lastObservedOpsCode[2] = j
+    parsedOpsCode = parser(lastObservedOpsCode)
+
+    if (parsedOpsCode[0] === 19690720) {
+      solutionFound = true
+      break
+    }
+  }
+
+  if (solutionFound) {
+    break
+  }
+}
+
+console.log(`Part 2: Values that generate output 19690720: ${i},${j}`)
