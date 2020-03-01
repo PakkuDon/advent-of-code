ae1c6a9751dd60d0293372ff2121b2910b8734ef
PakkuDon <pakku.pd@gmail.com>
2020-03-01 23:30:42 +1100

    Add solution for 2015, Day 2, Part 2

--- 2015/02-i-was-told-there-would-be-no-math/index.js
+++ 2015/02-i-was-told-there-would-be-no-math/index.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const path = require('path')
 const calculatePaperRequired = require('./01')
+const calculateRibbonRequired = require('./02')
 
 const puzzleInput = fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf8')
 const allGiftDimensions = puzzleInput
@@ -20,4 +21,9 @@ const totalWrappingPaperRequired = allGiftDimensions
   .map(giftDimension => calculatePaperRequired(giftDimension))
   .reduce((total, current) => total + current, 0)
 
+const totalRibbonRequired = allGiftDimensions
+  .map(giftDimension => calculateRibbonRequired(giftDimension))
+  .reduce((total, current) => total + current, 0)
+
 console.log(`Part 1: ${totalWrappingPaperRequired}`)
+console.log(`Part 2: ${totalRibbonRequired}`)
