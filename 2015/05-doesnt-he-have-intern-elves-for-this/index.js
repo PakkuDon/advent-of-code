81378310cfe7bafbdd2d656ea14da5a81bfb8f93
PakkuDon <pakku.pd@gmail.com>
2020-03-15 23:41:43 +1100

    Add solution for 2015, Day 5, Part 2

--- 2015/05-doesnt-he-have-intern-elves-for-this/index.js
+++ 2015/05-doesnt-he-have-intern-elves-for-this/index.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const path = require('path')
 const isNiceString = require('./01')
+const isNiceStringV2 = require('./02')
 
 const puzzleInput = fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf8')
 const strings = puzzleInput
@@ -8,11 +9,16 @@ const strings = puzzleInput
   .split('\n')
 
 let numberOfNiceStrings = 0
+let numberOfNiceStringsV2 = 0
 
 for (let string of strings) {
   if (isNiceString(string)) {
     numberOfNiceStrings++
   }
+  if (isNiceStringV2(string)) {
+    numberOfNiceStringsV2++
+  }
 }
 
 console.log(`Part 1: ${numberOfNiceStrings}`)
+console.log(`Part 2: ${numberOfNiceStringsV2}`)
