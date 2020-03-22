efff03dd48de85585aa92ad0a1406c5bd263136a
PakkuDon <pakku.pd@gmail.com>
2020-03-23 00:23:40 +1100

    Add solution for 2015, Day 6, Part 2

---
+++ 2015/06-probably-a-fire-hazard/02.js
@@ -0,0 +1,65 @@
+const GRID_LENGTH = 1000
+
+const parseInstruction = (instruction) => {
+  let [ _input, type, x1, y1, x2, y2 ] = instruction.match(/([A-Za-z ]+) (\d+),(\d+) through (\d+),(\d+)/)
+  return {
+    type,
+    x1: parseInt(x1, 10),
+    y1: parseInt(y1, 10),
+    x2: parseInt(x2, 10),
+    y2: parseInt(y2, 10),
+  }
+}
+
+const processInstructions = (grid, instructions) => {
+  instructions.forEach(instruction => {
+    const { type, x1, y1, x2, y2 } = parseInstruction(instruction)
+    let action
+
+    switch (type) {
+      case 'toggle':
+        action = (brightness) => brightness + 2
+        break
+      case 'turn on':
+        action = (brightness) => brightness + 1
+        break
+      case 'turn off':
+        action = (brightness) => {
+          if (brightness === 0) {
+            return brightness
+          }
+          return brightness - 1
+        }
+        break
+      default:
+        throw new Error(`Invalid action type. Received ${type}`)
+    }
+
+    for (let x = x1; x <= x2; x++) {
+      for (let y = y1; y <= y2; y++) {
+        grid[x][y] = action(grid[x][y])
+      }
+    }
+  })
+}
+
+const getTotalBrightness = (instructions) => {
+  let totalBrightness = 0
+  const grid = new Array(GRID_LENGTH)
+  for (let i = 0; i < grid.length; i++) {
+    grid[i] = new Array(GRID_LENGTH)
+    grid[i].fill(0)
+  }
+
+  processInstructions(grid, instructions)
+
+  for (let x = 0; x < grid.length; x++) {
+    for (let y = 0; y < grid[x].length; y++) {
+      totalBrightness += grid[x][y]
+    }
+  }
+
+  return totalBrightness
+}
+
+module.exports = getTotalBrightness
--- 2015/06-probably-a-fire-hazard/index.js
+++ 2015/06-probably-a-fire-hazard/index.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const path = require('path')
 const getNumberOfLightsSwitchedOn = require('./01')
+const getTotalBrightness = require('./02')
 
 const puzzleInput = fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf8')
 const instructions = puzzleInput
@@ -8,5 +9,7 @@ const instructions = puzzleInput
   .split('\n')
 
 const numberOfLightsSwitchedOn = getNumberOfLightsSwitchedOn(instructions)
+const totalBrightness = getTotalBrightness(instructions)
 
 console.log(`Part 1: ${numberOfLightsSwitchedOn}`)
+console.log(`Part 2: ${totalBrightness}`)
