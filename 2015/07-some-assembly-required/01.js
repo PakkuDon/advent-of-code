a46084fadb4ff8bf344804c315f38cbba7883a94
PakkuDon <pakku.pd@gmail.com>
2020-03-29 14:56:38 +1100

    Add solution for 2015, Day 7, Part 1

---
+++ 2015/07-some-assembly-required/01.js
@@ -0,0 +1,52 @@
+const MAX_VALUE = 65535
+
+const resolve = (signals, key) => {
+  if (!isNaN(key)) {
+    return key
+  }
+  else if (isNaN(signals[key])) {
+    signals[key] = signals[key]()
+    return signals[key]
+  }
+  return signals[key]
+}
+
+const calculateSignal = (instructions, target) => {
+  let signals = {}
+
+  for (let instruction of instructions) {
+    const [ _, input, key ] = instruction.match(/([\w\s]+)\ -> (\w+)/)
+
+    const tokens = input.split(' ')
+    if (tokens.length === 1) {
+      signals[key] = () => isNaN(tokens[0]) ? resolve(signals, tokens[0]) : parseInt(tokens[0], 10)
+    }
+    else if (tokens.length === 2) {
+      signals[key] = () => MAX_VALUE - resolve(signals, tokens[1])
+    }
+    else if (tokens.length === 3) {
+      const [ _, operandA, operator, operandB ] = input.match(/(\w+) (\w+) (\w+)/)
+      if (operator === 'AND') {
+        signals[key] = () => resolve(signals, operandA) & resolve(signals, operandB)
+      }
+      else if (operator === 'OR') {
+        signals[key] = () => resolve(signals, operandA) | resolve(signals, operandB)
+      }
+      else if (operator === 'LSHIFT') {
+        let shift = parseInt(operandB)
+        signals[key] = () => resolve(signals, operandA) << shift
+      }
+      else if (operator === 'RSHIFT') {
+        let shift = parseInt(operandB)
+        signals[key] = () => resolve(signals, operandA) >> shift
+      }
+      else {
+        throw new Error(`Invalid operator. Received ${operator}`)
+      }
+    }
+  }
+
+  return resolve(signals, target)
+}
+
+module.exports = calculateSignal
---
+++ 2015/07-some-assembly-required/index.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+const path = require('path')
+const calculateSignal = require('./01')
+
+const puzzleInput = fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf8')
+const instructions = puzzleInput
+  .trim()
+  .split('\n')
+
+const signal = calculateSignal(instructions, 'a')
+
+console.log(`Part 1: ${signal}`)
