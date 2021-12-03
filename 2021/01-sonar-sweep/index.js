4728b53f05e8825aa9e703a6c6ed175594508678
PakkuDon <pakku.pd@gmail.com>
2021-12-03 23:23:06 +1100

    Add solution for 2021, Day 1, Part 2

--- 2021/01-sonar-sweep/index.js
+++ 2021/01-sonar-sweep/index.js
@@ -1,8 +1,10 @@
 const fs = require('fs')
 const path = require('path')
 const countDepthIncreases = require('./01')
+const countDepthIncreasesWithWindow = require('./02')
 
 const puzzleInput = fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf8')
 const readings = puzzleInput.trim().split('\n').map(value => parseInt(value, 10))
 
 console.log(`Part 1: ${countDepthIncreases(readings)}`)
+console.log(`Part 2: ${countDepthIncreasesWithWindow(readings)}`)
