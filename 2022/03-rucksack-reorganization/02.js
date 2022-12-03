7d331878e928c18e15cb676d58c20607f57515ac
PakkuDon <pakku.pd@gmail.com>
2022-12-03 16:18:47 +1100

    Add solution for 2022, Day 3, Part 2

---
+++ 2022/03-rucksack-reorganization/02.js
@@ -0,0 +1,38 @@
+module.exports = (rucksacks) => {
+  let sum = 0
+  const sharedItemTypes = []
+  const groupedRucksacks = []
+
+  for (let i = 0; i < rucksacks.length; i += 3) {
+    groupedRucksacks.push(rucksacks.slice(i, i + 3))
+  }
+
+  groupedRucksacks.forEach(group => {
+    let foundSharedItem = false
+    for (let i = 0; i < group.length; i++) {
+      let rucksack = group[i]
+      for (let j = 0; j < rucksack.length; j++) {
+        let itemType = rucksack[j]
+        if (group[0].includes(itemType) && group[1].includes(itemType) && group[2].includes(itemType)) {
+          sharedItemTypes.push(itemType)
+          foundSharedItem = true
+          break
+        }
+      }
+
+      if (foundSharedItem) {
+        break
+      }
+    }
+  })
+
+  sharedItemTypes.forEach(itemType => {
+    if (itemType === itemType.toLowerCase()) {
+      sum += itemType.charCodeAt(0) - "a".charCodeAt(0) + 1
+    } else {
+      sum += itemType.charCodeAt(0) - "A".charCodeAt(0) + 27
+    }
+  })
+
+  return sum
+}
