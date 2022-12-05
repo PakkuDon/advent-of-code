15ad39e45774989e3974fd56a3d168e9a008540f
PakkuDon <pakku.pd@gmail.com>
2022-12-05 14:23:15 +1100

    Add boilerplate code for JS solutions

---
+++ template-js/solution.js
@@ -0,0 +1,12 @@
+const part1 = (values) => {
+  console.log("Not implemented yet")
+}
+
+const part2 = (values) => {
+  console.log("Not implemented yet")
+}
+
+module.exports = {
+  part1,
+  part2,
+}
---
+++ template-js/solution.test.js
@@ -0,0 +1,19 @@
+const { part1, part2 } = require('./solution')
+
+describe('part1', () => {
+  test('returns something', () => {
+    const input = [
+      "a value"
+    ]
+    expect(part1(input)).toEqual(0)
+  })
+})
+
+describe('part2', () => {
+  test('returns something', () => {
+    const input = [
+      "a value"
+    ]
+    expect(part2(input)).toEqual(0)
+  })
+})
