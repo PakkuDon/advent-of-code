4063d358b7d5adc128eaaac2d54c343f59f364a1
PakkuDon <pakku.pd@gmail.com>
2022-12-05 14:05:43 +1100

    Place solutions in same file for both parts of 2022 Day 3

--- 2022/03-rucksack-reorganization/solution.js
+++ 2022/03-rucksack-reorganization/solution.js
@@ -1,4 +1,34 @@
-module.exports = (rucksacks) => {
+const part1 = (rucksacks) => {
+  let sum = 0
+  const sharedItemTypes = []
+  
+  rucksacks.forEach(rucksack => {
+    let compartments = []
+    const midway = rucksack.length / 2
+    compartments.push(rucksack.slice(0, midway))
+    compartments.push(rucksack.slice(midway))
+
+    for (let i = 0; i < compartments[0].length; i++) {
+      let itemType = compartments[0][i]
+      if (compartments[1].includes(itemType)) {
+        sharedItemTypes.push(itemType)
+        break
+      }
+    }
+  })
+
+  sharedItemTypes.forEach(itemType => {
+    if (itemType === itemType.toLowerCase()) {
+      sum += itemType.charCodeAt(0) - "a".charCodeAt(0) + 1
+    } else {
+      sum += itemType.charCodeAt(0) - "A".charCodeAt(0) + 27
+    }
+  })
+
+  return sum
+}
+
+const part2 = (rucksacks) => {
   let sum = 0
   const sharedItemTypes = []
   const groupedRucksacks = []
@@ -35,4 +65,9 @@ module.exports = (rucksacks) => {
   })
 
   return sum
+}
+
+module.exports = {
+  part1,
+  part2,
 }
---
+++ 2022/03-rucksack-reorganization/solution.test.js
@@ -0,0 +1,29 @@
+const { part1, part2 } = require('./solution')
+
+describe('part1', () => {
+  test('returns sum of priority of item types', () => {
+    const input = [
+      "vJrwpWtwJgWrhcsFMMfFFhFp",
+      "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+      "PmmdzqPrVvPwwTWBwg",
+      "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+      "ttgJtRGJQctTZtZT",
+      "CrZsJsPPZsGzwwsLwLmpwMDw",
+    ]
+    expect(part1(input)).toEqual(157)
+  })
+})
+
+describe('part2', () => {
+  test('returns sum of priority of item types', () => {
+    const input = [
+      "vJrwpWtwJgWrhcsFMMfFFhFp",
+      "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+      "PmmdzqPrVvPwwTWBwg",
+      "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+      "ttgJtRGJQctTZtZT",
+      "CrZsJsPPZsGzwwsLwLmpwMDw",
+    ]
+    expect(part2(input)).toEqual(70)
+  })
+})
