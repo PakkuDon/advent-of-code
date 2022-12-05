5ea4f1d21539bf378aa6d732650b68a6c76fc5eb
PakkuDon <pakku.pd@gmail.com>
2022-12-05 13:27:37 +1100

    Place solutions in same file for both parts of 2022 Day 1

---
+++ 2022/01-calorie-counting/solution.js
@@ -0,0 +1,33 @@
+const part1 = (values) => {
+  const totalCaloriesByElves = []
+
+  values.forEach(value => {
+    if (!value) {
+      totalCaloriesByElves.push(0)
+    } else {
+      totalCaloriesByElves[totalCaloriesByElves.length - 1] += parseInt(value, 10)
+    }
+  })
+
+  return Math.max(...totalCaloriesByElves)
+}
+
+const part2 = (values) => {
+  const totalCaloriesByElves = []
+
+  values.forEach(value => {
+    if (!value) {
+      totalCaloriesByElves.push(0)
+    } else {
+      totalCaloriesByElves[totalCaloriesByElves.length - 1] += parseInt(value, 10)
+    }
+  })
+
+  totalCaloriesByElves.sort((a, b) => b - a)
+  return totalCaloriesByElves.slice(0, 3).reduce((total, current) => total + current)
+}
+
+module.exports = {
+  part1,
+  part2,
+}
---
+++ 2022/01-calorie-counting/solution.test.js
@@ -0,0 +1,45 @@
+const { part1, part2 } = require('./solution')
+
+describe('part1', () => {
+  test('returns highest number of calories carried by an elf', () => {
+    const input = [
+      "1000",
+      "2000",
+      "3000",
+      "",
+      "4000",
+      "",
+      "5000",
+      "6000",
+      "",
+      "7000",
+      "8000",
+      "9000",
+      "",
+      "10000",
+    ]
+    expect(part1(input)).toEqual(24000)
+  })
+})
+
+describe('part2', () => {
+  test('returns total number of calories carried by top three elves with the most calories', () => {
+    const input = [
+      "1000",
+      "2000",
+      "3000",
+      "",
+      "4000",
+      "",
+      "5000",
+      "6000",
+      "",
+      "7000",
+      "8000",
+      "9000",
+      "",
+      "10000",
+    ]
+    expect(part2(input)).toEqual(45000)
+  })
+})
