0cd5eaed6edf1d21120effc3ffe900d3d0cf03ce
PakkuDon <pakku.pd@gmail.com>
2022-12-07 18:23:40 +1100

    Refactor solution for 2022, Day 7

--- 2022/07-no-space-left-on-device/solution.js
+++ 2022/07-no-space-left-on-device/solution.js
@@ -1,4 +1,4 @@
-const part1 = (lines) => {
+const createFolders = (lines) => {
   const root = {
     totalSize: 0,
     children: [],
@@ -65,14 +65,7 @@ const part1 = (lines) => {
     folders[path].totalSize = calculateTotalSize(folders, path)
   })
 
-  const foldersBelowMaxSize = Object.keys(folders).filter(
-    (path) => folders[path].totalSize < 100000
-  )
-
-  return foldersBelowMaxSize.reduce(
-    (total, path) => total + folders[path].totalSize,
-    0
-  )
+  return folders
 }
 
 const calculateTotalSize = (folders, path) => {
@@ -88,72 +81,21 @@ const calculateTotalSize = (folders, path) => {
   return sum
 }
 
+const part1 = (lines) => {
+  const folders = createFolders(lines)
+
+  const foldersBelowMaxSize = Object.keys(folders).filter(
+    (path) => folders[path].totalSize < 100000
+  )
+
+  return foldersBelowMaxSize.reduce(
+    (total, path) => total + folders[path].totalSize,
+    0
+  )
+}
+
 const part2 = (lines) => {
-  const root = {
-    totalSize: 0,
-    children: [],
-    parent: null,
-    path: "/",
-  }
-  const folders = {
-    "/": root,
-  }
-  let currentFolder = null
-  let path = ""
-
-  for (let i = 0; i < lines.length; i++) {
-    const line = lines[i]
-    if (line[0] === "$") {
-      if (line.includes("cd")) {
-        const folderName = line.split("cd ")[1]
-        if (folderName === "/") {
-          path = "/"
-        } else if (folderName === "..") {
-          path = path.substring(0, path.lastIndexOf("/"))
-          currentFolder = folders[path]
-        } else {
-          path = `${path}/${folderName}`
-        }
-
-        if (!(folderName === "..") && !folders[path]) {
-          const newFolder = {
-            totalSize: 0,
-            children: [],
-            path: path,
-          }
-          folders[path] = newFolder
-        }
-
-        if (!(folderName === "..")) {
-          currentFolder = folders[path]
-        }
-      }
-    } else {
-      let files = []
-      while (i < lines.length && lines[i][0] !== "$") {
-        files.push(lines[i])
-        i++
-      }
-
-      files.forEach((file) => {
-        const fileInfo = file.split(" ")
-        if (fileInfo.includes("dir")) {
-          currentFolder.children.push(`${currentFolder.path}/${fileInfo[1]}`)
-        } else {
-          currentFolder.children.push({
-            name: fileInfo[1],
-            size: parseInt(fileInfo[0], 10),
-          })
-        }
-      })
-      // Hack to prevent double increment when encountering another command
-      i--
-    }
-  }
-
-  Object.keys(folders).forEach((path) => {
-    folders[path].totalSize = calculateTotalSize(folders, path)
-  })
+  const folders = createFolders(lines)
 
   const unusedSpace = 70000000 - folders["/"].totalSize
   const requiredUnusedSpace = 30000000
