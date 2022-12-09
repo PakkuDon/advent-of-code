562f3f57bcb9a038215dfdcf2cdfa2af963a20aa
PakkuDon <pakku.pd@gmail.com>
2022-12-10 00:35:04 +1100

    Refactor solution for 2022, Day 9

--- 2022/09-rope-bridge/solution.js
+++ 2022/09-rope-bridge/solution.js
@@ -1,54 +1,5 @@
-const isAdjacent = (positionA, positionB) => {
-  return (
-    Math.abs(positionA.x - positionB.x) <= 1 &&
-    Math.abs(positionA.y - positionB.y) <= 1
-  )
-}
-
-const part1 = (moves) => {
-  let head = { x: 0, y: 0 }
-  let tail = { x: 0, y: 0 }
-  const tilesVisited = []
-
-  tilesVisited.push({ ...tail })
-
-  moves.forEach((move) => {
-    const [direction, units] = move.split(" ")
-    let shift
-
-    switch (direction) {
-      case "R":
-        shift = (position) => ({ ...position, x: position.x + 1 })
-        break
-      case "U":
-        shift = (position) => ({ ...position, y: position.y + 1 })
-        break
-      case "L":
-        shift = (position) => ({ ...position, x: position.x - 1 })
-        break
-      case "D":
-        shift = (position) => ({ ...position, y: position.y - 1 })
-        break
-    }
-
-    for (let i = 0; i < units; i++) {
-      const newPosition = shift(head)
-      if (!isAdjacent(tail, newPosition)) {
-        tail = head
-      }
-      head = newPosition
-      tilesVisited.push({ ...tail })
-    }
-  })
-
-  return tilesVisited.reduce(
-    (uniqueTiles, current) => uniqueTiles.add(JSON.stringify(current)),
-    new Set()
-  ).size
-}
-
-const part2 = (moves) => {
-  const nodes = new Array(10).fill().map((_) => ({ x: 0, y: 0 }))
+const countUniqueCoordinatesVisitedByTail = (moves, knotCount) => {
+  const nodes = new Array(knotCount).fill().map((_) => ({ x: 0, y: 0 }))
   const tilesVisited = []
 
   tilesVisited.push({ ...nodes[nodes.length - 1] })
@@ -101,6 +52,21 @@ const part2 = (moves) => {
     (uniqueTiles, current) => uniqueTiles.add(JSON.stringify(current)),
     new Set()
   ).size
+}
+
+const isAdjacent = (positionA, positionB) => {
+  return (
+    Math.abs(positionA.x - positionB.x) <= 1 &&
+    Math.abs(positionA.y - positionB.y) <= 1
+  )
+}
+
+const part1 = (moves) => {
+  return countUniqueCoordinatesVisitedByTail(moves, 2)
+}
+
+const part2 = (moves) => {
+  return countUniqueCoordinatesVisitedByTail(moves, 10)
 }
 
 module.exports = {
