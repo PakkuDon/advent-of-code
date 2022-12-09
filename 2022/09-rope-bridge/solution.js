12c8bc507cae0e8413078256a11ff20871df33b1
PakkuDon <pakku.pd@gmail.com>
2022-12-10 00:31:46 +1100

    Add solution for 2022, Day 9, Part 2

--- 2022/09-rope-bridge/solution.js
+++ 2022/09-rope-bridge/solution.js
@@ -47,7 +47,61 @@ const part1 = (moves) => {
   ).size
 }
 
-const part2 = (values) => {}
+const part2 = (moves) => {
+  const nodes = new Array(10).fill().map((_) => ({ x: 0, y: 0 }))
+  const tilesVisited = []
+
+  tilesVisited.push({ ...nodes[nodes.length - 1] })
+
+  moves.forEach((move) => {
+    const [direction, units] = move.split(" ")
+    let shift
+
+    switch (direction) {
+      case "R":
+        shift = (position) => ({ ...position, x: position.x + 1 })
+        break
+      case "U":
+        shift = (position) => ({ ...position, y: position.y + 1 })
+        break
+      case "L":
+        shift = (position) => ({ ...position, x: position.x - 1 })
+        break
+      case "D":
+        shift = (position) => ({ ...position, y: position.y - 1 })
+        break
+    }
+
+    for (let i = 0; i < units; i++) {
+      nodes[0] = shift(nodes[0])
+
+      for (let i = 1; i < nodes.length; i++) {
+        const current = nodes[i]
+        const previous = nodes[i - 1]
+        if (!isAdjacent(current, previous)) {
+          if (current.x <= previous.x - 1) {
+            current.x++
+          } else if (current.x >= previous.x + 1) {
+            current.x--
+          }
+
+          if (current.y <= previous.y - 1) {
+            current.y++
+          } else if (current.y >= previous.y + 1) {
+            current.y--
+          }
+        }
+      }
+
+      tilesVisited.push({ ...nodes[nodes.length - 1] })
+    }
+  })
+
+  return tilesVisited.reduce(
+    (uniqueTiles, current) => uniqueTiles.add(JSON.stringify(current)),
+    new Set()
+  ).size
+}
 
 module.exports = {
   part1,
--- 2022/09-rope-bridge/solution.test.js
+++ 2022/09-rope-bridge/solution.test.js
@@ -7,9 +7,14 @@ describe("part1", () => {
   })
 })
 
-xdescribe("part2", () => {
-  test("returns something", () => {
-    const input = ["a value"]
-    expect(part2(input)).toEqual(0)
+describe("part2", () => {
+  test("returns count of unique coordinates visited by tail of rope with 10 knots", () => {
+    const input = ["R 4", "U 4", "L 3", "D 1", "R 4", "D 1", "L 5", "R 2"]
+    expect(part2(input)).toEqual(1)
+  })
+
+  test("larger sample", () => {
+    const input = ["R 5", "U 8", "L 8", "D 3", "R 17", "D 10", "L 25", "U 20"]
+    expect(part2(input)).toEqual(36)
   })
 })
