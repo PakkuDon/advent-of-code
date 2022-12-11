1ad82c57a8eeb5ae7ee5b2385f95b05b0105b94d
PakkuDon <pakku.pd@gmail.com>
2022-12-12 00:15:01 +1100

    Add solution for 2016, Day 3, Part 2

--- 2016/03-squares-with-three-sides/solution.js
+++ 2016/03-squares-with-three-sides/solution.js
@@ -9,7 +9,25 @@ const part1 = (rows) =>
     return a + b > c
   }).length
 
-const part2 = (values) => {}
+const part2 = (rows) => {
+  const trianglesLengths = []
+  for (let i = 0; i < rows.length; i += 3) {
+    const matrix = rows.map((row) =>
+      row
+        .trim()
+        .split(/\s+/g)
+        .map((value) => parseInt(value, 10))
+    )
+    for (let j = 0; j < 3; j++) {
+      trianglesLengths.push([matrix[i][j], matrix[i + 1][j], matrix[i + 2][j]])
+    }
+  }
+
+  return trianglesLengths.filter((lengths) => {
+    const [a, b, c] = lengths.sort((a, b) => (a > b ? 1 : -1))
+    return a + b > c
+  }).length
+}
 
 module.exports = {
   part1,
--- 2016/03-squares-with-three-sides/solution.test.js
+++ 2016/03-squares-with-three-sides/solution.test.js
@@ -7,9 +7,16 @@ describe("part1", () => {
   })
 })
 
-xdescribe("part2", () => {
-  test("returns something", () => {
-    const input = ["a value"]
-    expect(part2(input)).toEqual(0)
+describe("part2", () => {
+  test("returns count of triangles with valid sides when lengths counted vertically", () => {
+    const input = [
+      "101 301 501",
+      "102 302 502",
+      "103 303 503",
+      "201 401 601",
+      "202 402 602",
+      "203 403 603",
+    ]
+    expect(part2(input)).toEqual(6)
   })
 })
