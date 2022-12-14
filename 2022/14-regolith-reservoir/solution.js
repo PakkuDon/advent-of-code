1dd294b6f42e5ba92f0d43ff47041625bc234f12
PakkuDon <pakku.pd@gmail.com>
2022-12-14 18:49:35 +1100

    Optimise solution for 2022, Day 14, Part 2

--- 2022/14-regolith-reservoir/solution.js
+++ 2022/14-regolith-reservoir/solution.js
@@ -82,8 +82,9 @@ const part1 = (input) => {
 const part2 = (input) => {
   const SAND_ORIGIN = { x: 500, y: 0 }
   let sandUnits = 0
+  let cavernHeight = 0
 
-  const occupiedTiles = []
+  const occupiedTiles = {}
   const rows = input.trim().split("\n")
   rows.forEach((row) => {
     const coordinates = row.split(" -> ")
@@ -96,62 +97,57 @@ const part2 = (input) => {
         .split(",")
         .map((value) => parseInt(value, 10))
 
+      if (Math.max(y1, y2) > cavernHeight) {
+        cavernHeight = Math.max(y1, y2)
+      }
+
       if (x1 !== x2) {
         let start = Math.min(x1, x2)
         let end = Math.max(x1, x2)
-        for (let j = start; j <= end; j++) {
-          occupiedTiles.push({ x: j, y: y1 })
+        for (let x = start; x <= end; x++) {
+          occupiedTiles[`${x},${y1}`] = true
         }
       } else {
         let start = Math.min(y1, y2)
         let end = Math.max(y1, y2)
-        for (let j = start; j <= end; j++) {
-          occupiedTiles.push({ x: x1, y: j })
+        for (let y = start; y <= end; y++) {
+          occupiedTiles[`${x1},${y}`] = true
         }
       }
     }
   })
 
   // Add floor
-  let cavernHeight = Math.max(
-    ...occupiedTiles.map((coordinate) => coordinate.y)
-  )
   // Doesn't quite span infinity but covers enough ground given puzzle input
   for (let x = 0; x < 1000; x++) {
-    occupiedTiles.push({ y: cavernHeight + 2, x })
+    occupiedTiles[`${x},${cavernHeight + 2}`] = true
   }
   let current = { ...SAND_ORIGIN }
 
   while (true) {
     // If next tile is clear move down
     if (
-      !occupiedTiles.find(
-        (tile) => tile.x === current.x && tile.y === current.y + 1
-      )
+      !occupiedTiles[`${current.x},${current.y + 1}`]
     ) {
       current.y++
     }
     // Else if down-left tile is clear move down-left
     else if (
-      !occupiedTiles.find(
-        (tile) => tile.x === current.x - 1 && tile.y === current.y + 1
-      )
+      !occupiedTiles[`${current.x - 1},${current.y + 1}`]
     ) {
       current.y++
       current.x--
     }
     // Else if down-right tile is clear move down-right
     else if (
-      !occupiedTiles.find(
-        (tile) => tile.x === current.x + 1 && tile.y === current.y + 1
-      )
+      !occupiedTiles[`${current.x + 1},${current.y + 1}`]
     ) {
       current.y++
       current.x++
     }
     // Else break and start next unit
     else {
-      occupiedTiles.push(current)
+      occupiedTiles[`${current.x},${current.y}`] = true
       sandUnits++
       if (current.x === SAND_ORIGIN.x && current.y === SAND_ORIGIN.y) {
         break
