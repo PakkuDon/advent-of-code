92df44713ad485e83b507d9eb9fa1cbc2fb46591
PakkuDon <pakku.pd@gmail.com>
2022-12-14 18:41:58 +1100

    Add solution for 2022, Day 14, Part 2

--- 2022/14-regolith-reservoir/solution.test.js
+++ 2022/14-regolith-reservoir/solution.test.js
@@ -8,9 +8,10 @@ describe("part1", () => {
   })
 })
 
-xdescribe("part2", () => {
-  test("returns something", () => {
-    const input = ["a value"]
-    expect(part2(input)).toEqual(0)
+describe("part2", () => {
+  test("returns how many units of sand come to a rest given an infinite floor", () => {
+    const input = `498,4 -> 498,6 -> 496,6
+503,4 -> 502,4 -> 502,9 -> 494,9`
+    expect(part2(input)).toEqual(93)
   })
 })
