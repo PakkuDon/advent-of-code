c2cab6f09ce18a72edcf35268855f8dadc33ca33
PakkuDon <pakku.pd@gmail.com>
2023-11-29 18:23:22 +1100

    Refactor solution for 2020, Day 3

--- 2020/03-toboggan-trajectory/solution.js
+++ 2020/03-toboggan-trajectory/solution.js
@@ -1,11 +1,10 @@
-const part1 = (input) => {
-  const grid = input.split("\n").map((row) => row.split(""))
+const countTrees = (grid, slope) => {
   let count = 0
-  const current = { x: 0, y: 0 }
+  let current = { x: 0, y: 0 }
 
   while (current.y < grid.length - 1) {
-    current.x = (current.x + 3) % grid[current.y].length
-    current.y++
+    current = slope(current)
+    current.x = current.x % grid[0].length
 
     if (grid[current.y][current.x] === "#") {
       count++
@@ -13,6 +12,14 @@ const part1 = (input) => {
   }
 
   return count
+}
+
+const part1 = (input) => {
+  const grid = input.split("\n").map((row) => row.split(""))
+  return countTrees(grid, (position) => ({
+    x: position.x + 3,
+    y: position.y + 1,
+  }))
 }
 
 const part2 = (input) => {
@@ -28,17 +35,7 @@ const part2 = (input) => {
   ]
 
   slopes.forEach((slope) => {
-    let treeCount = 0
-    let current = { x: 0, y: 0 }
-    while (current.y < grid.length - 1) {
-      current = slope(current)
-      current.x = current.x % grid[0].length
-
-      if (grid[current.y][current.x] === "#") {
-        treeCount++
-      }
-    }
-    counts.push(treeCount)
+    counts.push(countTrees(grid, slope))
   })
 
   return counts.reduce((total, current) => total * current)
