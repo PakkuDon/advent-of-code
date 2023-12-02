7407e76af901c45fdd0becd4c69903c71b5c6695
PakkuDon <pakku.pd@gmail.com>
2023-12-02 16:38:45 +1100

    Add solution for 2023, Day 2, Part 2

--- 2023/02-cube-conundrum/solution.js
+++ 2023/02-cube-conundrum/solution.js
@@ -30,6 +30,41 @@ const part1 = (input) => {
 }
 
 const part2 = (input) => {
+  const games = input.split("\n").map((gameDetails) => {
+    const rounds = gameDetails
+      .split(": ")[1]
+      .split(";")
+      .map((round) => {
+        const pulls = { red: 0, green: 0, blue: 0 }
+        const moves = round.trim().split(",")
+        moves.forEach((move) => {
+          let [number, colour] = move.trim().split(" ")
+          number = parseInt(number, 10)
+          pulls[colour] += number
+        })
+        return pulls
+      })
+
+    return { rounds }
+  })
+
+  const leastNumberOfCubes = games.map((game) => {
+    const requiredGreens = Math.max(...game.rounds.map((round) => round.green))
+    const requiredBlues = Math.max(...game.rounds.map((round) => round.blue))
+    const requiredReds = Math.max(...game.rounds.map((round) => round.red))
+
+    return {
+      requiredGreens,
+      requiredBlues,
+      requiredReds,
+    }
+  })
+
+  const powers = leastNumberOfCubes.map(
+    ({ requiredGreens, requiredBlues, requiredReds }) =>
+      requiredGreens * requiredBlues * requiredReds
+  )
+  return powers.reduce((total, current) => total + current)
 }
 
 module.exports = {
