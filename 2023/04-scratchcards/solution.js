ba501fa69501ad69fdffe5a1f74ab22400b931a8
PakkuDon <pakku.pd@gmail.com>
2023-12-04 17:34:40 +1100

    Add solution for 2023, Day 4, Part 2

--- 2023/04-scratchcards/solution.js
+++ 2023/04-scratchcards/solution.js
@@ -29,7 +29,46 @@ const part1 = (input) => {
   return sum
 }
 
-const part2 = (input) => {}
+const part2 = (input) => {
+  const rows = input.split("\n")
+  let cards = rows.map((row) => {
+    const number = parseInt(row.split(":")[0].match(/\d+/)[0], 10)
+    const [winningInput, numbersInput] = row.split(": ")[1].split(" | ")
+    const winningNumbers = winningInput
+      .trim()
+      .split(/\s+/)
+      .map((number) => parseInt(number, 10))
+    const numbersYouHave = numbersInput
+      .trim()
+      .split(/\s+/)
+      .map((number) => parseInt(number, 10))
+    const matchCount = winningNumbers.filter((number) =>
+      numbersYouHave.includes(number)
+    ).length
+
+    return { number, matchCount }
+  })
+
+  cards.forEach((card) => {
+    const cardIdsToAdd = []
+    for (let i = 1; i <= card.matchCount; i++) {
+      cardIdsToAdd.push(card.number + i)
+    }
+    const cardsToAdd = cards.filter((card) =>
+      cardIdsToAdd.includes(card.number)
+    )
+    card.cardsToAdd = cardsToAdd
+  })
+
+  let count = 0
+  while (cards.length > 0) {
+    const card = cards[0]
+    cards = [...card.cardsToAdd, ...cards.slice(1)]
+    count++
+  }
+
+  return count
+}
 
 module.exports = {
   part1,
