061aebce1c8a93eef85efe27d789e8d4609888c5
PakkuDon <pakku.pd@gmail.com>
2023-12-04 12:10:35 +1100

    Reduce number of if statements in 2023, Day 1, Part 2

--- 2023/01/solution.js
+++ 2023/01/solution.js
@@ -19,6 +19,15 @@ const part2 = (input) => {
   const rows = input.split("\n")
   let extractedDigits = []
   const wordsToDigits = {
+    1: 1,
+    2: 2,
+    3: 3,
+    4: 4,
+    5: 5,
+    6: 6,
+    7: 7,
+    8: 8,
+    9: 9,
     one: 1,
     two: 2,
     three: 3,
@@ -34,15 +43,8 @@ const part2 = (input) => {
     const digits = row.match(
       /(\d|(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine))/g
     )
-    let firstDigit = digits[0]
-    let lastDigit = digits[digits.length - 1]
-
-    if (isNaN(firstDigit)) {
-      firstDigit = wordsToDigits[firstDigit]
-    }
-    if (isNaN(lastDigit)) {
-      lastDigit = wordsToDigits[lastDigit]
-    }
+    let firstDigit = wordsToDigits[digits[0]]
+    let lastDigit = wordsToDigits[digits[digits.length - 1]]
 
     extractedDigits.push(firstDigit + "" + lastDigit)
   })
