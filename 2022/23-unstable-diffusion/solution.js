03a75e491bc6b1bb771444a98f07cef046154e05
PakkuDon <pakku.pd@gmail.com>
2023-12-06 11:29:59 +1100

    Add solution for 2022, Day 23, Part 2

--- 2022/23-unstable-diffusion/solution.js
+++ 2022/23-unstable-diffusion/solution.js
@@ -131,7 +131,126 @@ const part1 = (input) => {
   )
 }
 
-const part2 = (input) => {}
+const part2 = (input) => {
+  const elves = []
+  const map = {}
+
+  // Get initial elf locations
+  input.split("\n").forEach((row, index) => {
+    for (let x = 0; x < row.length; x++) {
+      if (row[x] === "#") {
+        const elf = { x, y: index, nextMove: "" }
+
+        elves.push(elf)
+        map[`${x},${index}`] = elf
+      }
+    }
+  })
+
+  // Process rounds until no elves have any next moves recorded
+  const movesByPreference = [
+    {
+      side: "north",
+      getNextPosition: ({ x, y }) => `${x},${y - 1}`,
+    },
+    {
+      side: "south",
+      getNextPosition: ({ x, y }) => `${x},${y + 1}`,
+    },
+    {
+      side: "west",
+      getNextPosition: ({ x, y }) => `${x - 1},${y}`,
+    },
+    {
+      side: "east",
+      getNextPosition: ({ x, y }) => `${x + 1},${y}`,
+    },
+  ]
+
+  let roundNo = 0
+  while (true) {
+    const nextMoves = {}
+    // First determine move for each elf
+    elves.forEach((elf) => {
+      const { x, y } = elf
+      let nextPosition
+      const north = [
+        map[`${x - 1},${y - 1}`],
+        map[`${x},${y - 1}`],
+        map[`${x + 1},${y - 1}`],
+      ]
+      const west = [
+        map[`${x - 1},${y - 1}`],
+        map[`${x - 1},${y}`],
+        map[`${x - 1},${y + 1}`],
+      ]
+      const south = [
+        map[`${x - 1},${y + 1}`],
+        map[`${x},${y + 1}`],
+        map[`${x + 1},${y + 1}`],
+      ]
+      const east = [
+        map[`${x + 1},${y - 1}`],
+        map[`${x + 1},${y}`],
+        map[`${x + 1},${y + 1}`],
+      ]
+
+      // If there are no adjacent elves do nothing
+      // Otherwise set new position for elf
+      if (
+        [...north, ...east, ...south, ...west].filter((cell) => cell).length > 0
+      ) {
+        const neighbours = {
+          north,
+          east,
+          west,
+          south,
+        }
+        for (let i = 0; i < movesByPreference.length; i++) {
+          const moveToConsider =
+            movesByPreference[(i + roundNo) % movesByPreference.length]
+          const neighboursInDirection = neighbours[moveToConsider.side].filter(
+            (cell) => cell
+          )
+          if (neighboursInDirection.length === 0) {
+            nextPosition = moveToConsider.getNextPosition(elf)
+            break
+          }
+        }
+
+        if (nextPosition) {
+          if (!nextMoves[nextPosition]) {
+            nextMoves[nextPosition] = []
+          }
+          nextMoves[nextPosition].push(elf)
+          elf.nextMove = nextPosition
+        }
+      }
+    })
+
+    roundNo++
+    if (elves.every((elf) => elf.nextMove === "")) {
+      return roundNo
+    }
+
+    // Move elves if new move is valid
+    elves.forEach((elf) => {
+      if (elf.nextMove) {
+        if ((nextMoves[elf.nextMove] || []).length === 1) {
+          const [newX, newY] = elf.nextMove
+            .split(",")
+            .map((coord) => parseInt(coord, 10))
+          map[`${elf.x},${elf.y}`] = null
+          map[`${newX},${newY}`] = elf
+          elf.x = newX
+          elf.y = newY
+        }
+      }
+
+      elf.nextMove = ""
+    })
+  }
+}
 
 module.exports = {
   part1,
--- 2022/23-unstable-diffusion/solution.test.js
+++ 2022/23-unstable-diffusion/solution.test.js
@@ -13,9 +13,15 @@ describe("part1", () => {
   })
 })
 
-xdescribe("part2", () => {
-  test("returns something", () => {
-    const input = `puzzle input`
-    expect(part2(input)).toEqual(0)
+describe("part2", () => {
+  test("returns first round where no elves move", () => {
+    const input = `....#..
+..###.#
+#...#.#
+.#...##
+#.###..
+##.#.##
+.#..#..`
+    expect(part2(input)).toEqual(20)
   })
 })
