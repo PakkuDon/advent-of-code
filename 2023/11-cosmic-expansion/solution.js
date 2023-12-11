79fd7e70969ef9ade0f0013004b9264e1458de9d
PakkuDon <pakku.pd@gmail.com>
2023-12-11 17:09:56 +1100

    Refactor solution for 2023, Day 11

--- 2023/11-cosmic-expansion/solution.js
+++ 2023/11-cosmic-expansion/solution.js
@@ -1,81 +1,22 @@
-const part1 = (input) => {
+const getGalaxies = (input) => {
   const galaxies = []
-  const grid = []
-  let sum = 0
-  // Parse input
   input
     .trim()
     .split("\n")
-    .forEach((row) => {
-      // Expand empty rows
-      if (!row.includes("#")) {
-        grid.push(row.split(""))
+    .forEach((row, y) => {
+      const cells = row.split("")
+      for (let x = 0; x < cells.length; x++) {
+        if (cells[x] === "#") {
+          galaxies.push({ x, y, originalX: x, originalY: y })
+        }
       }
-      grid.push(row.split(""))
     })
 
-  // Expand empty columns
-  for (let x = 0; x < grid[0].length; x++) {
-    const column = []
-    for (let y = 0; y < grid.length; y++) {
-      column.push(grid[y][x])
-    }
-    if (column.every((char) => char === ".")) {
-      for (let y = 0; y < grid.length; y++) {
-        grid[y].splice(x, 0, ".")
-      }
-      // Increment x to avoid loop from processing same column
-      x++
-    }
-  }
-
-  // Locate galaxies
-  for (let y = 0; y < grid.length; y++) {
-    for (let x = 0; x < grid[y].length; x++) {
-      if (grid[y][x] === "#") {
-        galaxies.push({ x, y })
-      }
-    }
-  }
-
-  // Generate possible pairs
-  const pairs = new Set()
-  for (let i = 0; i < galaxies.length; i++) {
-    for (let j = 0; j < galaxies.length; j++) {
-      if (i === j) {
-        continue
-      }
-      // Sort ints to prevent same pair from being processed twice
-      pairs.add([i, j].sort().join(","))
-    }
-  }
-
-  // Calculate sum of distances from each galaxy to another
-  const distances = []
-  pairs.forEach((pair) => {
-    const [a, b] = pair.split(",").map((value) => parseInt(value, 10))
-    const galaxy = galaxies[a]
-    const other = galaxies[b]
-    distances.push(Math.abs(galaxy.x - other.x) + Math.abs(galaxy.y - other.y))
-  })
-
-  return distances.reduce((total, current) => total + current, 0)
+  return galaxies
 }
 
-const part2 = (input, growth) => {
-  const galaxies = []
+const expandCoordinates = (input, galaxies, growth) => {
   const grid = input.trim().split("\n")
-
-  // Parse galaxies
-  grid.forEach((row, y) => {
-    const cells = row.split("")
-    for (let x = 0; x < cells.length; x++) {
-      if (cells[x] === "#") {
-        galaxies.push({ x, y, originalX: x, originalY: y })
-      }
-    }
-  })
-
   // Expand y coordinates
   for (let y = 0; y < grid.length; y++) {
     if (!grid[y].includes("#")) {
@@ -86,6 +27,7 @@ const part2 = (input, growth) => {
         })
     }
   }
+
   // Expand x coordinates
   for (let x = 0; x < grid[0].length; x++) {
     const column = []
@@ -100,6 +42,13 @@ const part2 = (input, growth) => {
         })
     }
   }
+}
+
+const getDistance = (a, b) => Math.abs(a.x - b.x) + Math.abs(a.y - b.y)
+
+const part1 = (input) => {
+  const galaxies = getGalaxies(input)
+  expandCoordinates(input, galaxies, 2)
 
   // Generate possible pairs
   const pairs = new Set()
@@ -117,9 +66,34 @@ const part2 = (input, growth) => {
   const distances = []
   pairs.forEach((pair) => {
     const [a, b] = pair.split(",").map((value) => parseInt(value, 10))
-    const galaxy = galaxies[a]
-    const other = galaxies[b]
-    const distance = Math.abs(galaxy.x - other.x) + Math.abs(galaxy.y - other.y)
+    const distance = getDistance(galaxies[a], galaxies[b])
+    distances.push(distance)
+  })
+
+  return distances.reduce((total, current) => total + current, 0)
+}
+
+const part2 = (input, growth) => {
+  const galaxies = getGalaxies(input)
+  expandCoordinates(input, galaxies, growth)
+
+  // Generate possible pairs
+  const pairs = new Set()
+  for (let i = 0; i < galaxies.length; i++) {
+    for (let j = 0; j < galaxies.length; j++) {
+      if (i === j) {
+        continue
+      }
+      // Sort ints to prevent same pair from being processed twice
+      pairs.add([i, j].sort().join(","))
+    }
+  }
+
+  // Calculate sum of distances from each galaxy to another
+  const distances = []
+  pairs.forEach((pair) => {
+    const [a, b] = pair.split(",").map((value) => parseInt(value, 10))
+    const distance = getDistance(galaxies[a], galaxies[b])
     distances.push(distance)
   })
   return distances.reduce((total, current) => total + current, 0)
