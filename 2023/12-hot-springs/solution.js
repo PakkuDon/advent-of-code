193185360acfff3073c79cb5a4089644fff41a9c
PakkuDon <pakku.pd@gmail.com>
2023-12-13 15:14:08 +1100

    Extract function for counting arrangements for 2023, Day 12
    This will be reused in Part 2

--- 2023/12-hot-springs/solution.js
+++ 2023/12-hot-springs/solution.js
@@ -1,3 +1,39 @@
+const countPossibleArrangements = (sequence, requiredSpringCount) => {
+  let count = 0
+  // Find position of unknown elements (?)
+  const unknownIndexes = []
+  for (let i = 0; i < sequence.length; i++) {
+    if (sequence[i] === "?") {
+      unknownIndexes.push(i)
+    }
+  }
+
+  // Generate permutations
+  for (let i = 0; i < Math.pow(2, unknownIndexes.length); i++) {
+    const mask = i.toString(2).padStart(unknownIndexes.length, "0")
+    let arrangement = [...sequence]
+
+    mask.split("").forEach((bit, index) => {
+      let char = bit === "0" ? "." : "#"
+      arrangement[unknownIndexes[index]] = char
+    })
+
+    const counts = arrangement
+      .join("")
+      .split(".")
+      .filter((group) => group)
+      .map((group) => group.length)
+    if (
+      counts.length === requiredSpringCount.length &&
+      counts.every((count, index) => count === requiredSpringCount[index])
+    ) {
+      count++
+    }
+  }
+
+  return count
+}
+
 const part1 = (input) => {
   let count = 0
 
@@ -10,36 +46,7 @@ const part1 = (input) => {
         .split(",")
         .map((value) => parseInt(value, 10))
 
-      // Find position of unknown elements (?)
-      const unknownIndexes = []
-      for (let i = 0; i < sequence.length; i++) {
-        if (sequence[i] === "?") {
-          unknownIndexes.push(i)
-        }
-      }
-
-      // Generate permutations
-      for (let i = 0; i < Math.pow(2, unknownIndexes.length); i++) {
-        const mask = i.toString(2).padStart(unknownIndexes.length, "0")
-        let arrangement = [...sequence]
-
-        mask.split("").forEach((bit, index) => {
-          let char = bit === "0" ? "." : "#"
-          arrangement[unknownIndexes[index]] = char
-        })
-
-        const counts = arrangement
-          .join("")
-          .split(".")
-          .filter((group) => group)
-          .map((group) => group.length)
-        if (
-          counts.length === requiredSpringCount.length &&
-          counts.every((count, index) => count === requiredSpringCount[index])
-        ) {
-          count++
-        }
-      }
+      count += countPossibleArrangements(sequence, requiredSpringCount)
     })
 
   return count
