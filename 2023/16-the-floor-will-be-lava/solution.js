dfa6164fda2d66310eb4b824d7079de708538844
PakkuDon <pakku.pd@gmail.com>
2023-12-16 19:56:49 +1100

    Add solution for 2023, Day 16, Part 2

--- 2023/16-the-floor-will-be-lava/solution.js
+++ 2023/16-the-floor-will-be-lava/solution.js
@@ -1,13 +1,9 @@
-const part1 = (input) => {
-  const grid = input
-    .trim()
-    .split("\n")
-    .map((row) => row.split(""))
-
+const getTilesEnergised = (grid, initialBeam) => {
   // Follow beam path(s)
   const visitedTiles = new Set()
-  const beams = []
-  beams.push({ x: 0, y: 0, direction: "right", state: "active" })
+  const visitedTilesWithDirection = new Set()
+  let beams = []
+  beams.push({ ...initialBeam, state: "active" })
   const moves = {
     right: {
       shift: ({ x, y }) => ({ x: x + 1, y }),
@@ -23,18 +19,7 @@ const part1 = (input) => {
     },
   }
 
-  let previousSetSize = 0
-  let cyclesSinceLastChange = 0
   while (beams.some((beam) => beam.state === "active")) {
-    // Exit once cycle detected
-    if (previousSetSize === visitedTiles.size) {
-      cyclesSinceLastChange++
-    }
-    previousSetSize = visitedTiles.size
-
-    if (cyclesSinceLastChange > 5) {
-      break
-    }
     beams
       .filter((beam) => beam.state === "active")
       .forEach((beam) => {
@@ -46,78 +31,90 @@ const part1 = (input) => {
           beam.x < grid[0].length
         ) {
           visitedTiles.add(`${beam.x},${beam.y}`)
-          const currentTile = grid[beam.y][beam.x]
-          let nextPosition
-          switch (currentTile) {
-            case ".":
-              nextPosition = moves[beam.direction].shift(beam)
-              beam.x = nextPosition.x
-              beam.y = nextPosition.y
-              break
-            case "/":
-              if (beam.direction === "right") {
-                beam.direction = "up"
-                beam.y--
-              } else if (beam.direction === "left") {
-                beam.direction = "down"
-                beam.y++
-              } else if (beam.direction === "up") {
-                beam.direction = "right"
-                beam.x++
-              } else if (beam.direction === "down") {
-                beam.direction = "left"
-                beam.x--
-              }
-              break
-            case "\\":
-              if (beam.direction === "left") {
-                beam.direction = "up"
-                beam.y--
-              } else if (beam.direction === "right") {
-                beam.direction = "down"
-                beam.y++
-              } else if (beam.direction === "up") {
-                beam.direction = "left"
-                beam.x--
-              } else if (beam.direction === "down") {
-                beam.direction = "right"
-                beam.x++
-              }
-              break
-            case "|":
-              if (beam.direction === "down" || beam.direction === "up") {
+          // Mark beam if complete if tile and direction already processed
+          if (
+            visitedTilesWithDirection.has(
+              `${beam.direction},${beam.x},${beam.y}`
+            )
+          ) {
+            beam.state = "complete"
+          } else {
+            visitedTilesWithDirection.add(
+              `${beam.direction},${beam.x},${beam.y}`
+            )
+            const currentTile = grid[beam.y][beam.x]
+            let nextPosition
+            switch (currentTile) {
+              case ".":
                 nextPosition = moves[beam.direction].shift(beam)
                 beam.x = nextPosition.x
                 beam.y = nextPosition.y
-              } else {
-                // Split beam
-                beams.push({
-                  x: beam.x,
-                  y: beam.y + 1,
-                  direction: "down",
-                  state: "active",
-                })
-                beam.direction = "up"
-                beam.y--
-              }
-              break
-            case "-":
-              if (beam.direction === "left" || beam.direction === "right") {
-                nextPosition = moves[beam.direction].shift(beam)
-                beam.x = nextPosition.x
-                beam.y = nextPosition.y
-              } else {
-                // Split beam
-                beams.push({
-                  x: beam.x + 1,
-                  y: beam.y,
-                  direction: "right",
-                  state: "active",
-                })
-                beam.direction = "left"
-                beam.x--
-              }
-              break
+                break
+              case "/":
+                if (beam.direction === "right") {
+                  beam.direction = "up"
+                  beam.y--
+                } else if (beam.direction === "left") {
+                  beam.direction = "down"
+                  beam.y++
+                } else if (beam.direction === "up") {
+                  beam.direction = "right"
+                  beam.x++
+                } else if (beam.direction === "down") {
+                  beam.direction = "left"
+                  beam.x--
+                }
+                break
+              case "\\":
+                if (beam.direction === "left") {
+                  beam.direction = "up"
+                  beam.y--
+                } else if (beam.direction === "right") {
+                  beam.direction = "down"
+                  beam.y++
+                } else if (beam.direction === "up") {
+                  beam.direction = "left"
+                  beam.x--
+                } else if (beam.direction === "down") {
+                  beam.direction = "right"
+                  beam.x++
+                }
+                break
+              case "|":
+                if (beam.direction === "down" || beam.direction === "up") {
+                  nextPosition = moves[beam.direction].shift(beam)
+                  beam.x = nextPosition.x
+                  beam.y = nextPosition.y
+                } else {
+                  // Split beam
+                  beams.push({
+                    x: beam.x,
+                    y: beam.y + 1,
+                    direction: "down",
+                    state: "active",
+                  })
+                  beam.direction = "up"
+                  beam.y--
+                }
+                break
+              case "-":
+                if (beam.direction === "left" || beam.direction === "right") {
+                  nextPosition = moves[beam.direction].shift(beam)
+                  beam.x = nextPosition.x
+                  beam.y = nextPosition.y
+                } else {
+                  // Split beam
+                  beams.push({
+                    x: beam.x + 1,
+                    y: beam.y,
+                    direction: "right",
+                    state: "active",
+                  })
+                  beam.direction = "left"
+                  beam.x--
+                }
+                break
+            }
           }
         } else {
           beam.state = "complete"
@@ -125,11 +122,48 @@ const part1 = (input) => {
       })
   }
 
-  // Count how many tiles have been energised
   return visitedTiles.size
 }
 
-const part2 = (input) => {}
+const part1 = (input) => {
+  const grid = input
+    .trim()
+    .split("\n")
+    .map((row) => row.split(""))
+
+  const beam = { x: 0, y: 0, direction: "right", state: "active" }
+  return getTilesEnergised(grid, beam)
+}
+
+const part2 = (input) => {
+  const grid = input
+    .trim()
+    .split("\n")
+    .map((row) => row.split(""))
+
+  const beams = []
+  // Push beams for vertical edges
+  for (let y = 0; y < grid.length; y++) {
+    beams.push({ x: 0, y, direction: "right" })
+    beams.push({ x: grid[0].length - 1, y, direction: "left" })
+  }
+
+  // Push beams for horizontal edges
+  for (let x = 0; x < grid[0].length; x++) {
+    beams.push({ x, y: 0, direction: "down" })
+    beams.push({ x, y: grid[0].length - 1, direction: "up" })
+  }
+
+  let maxTiles = 0
+  beams.forEach((beam) => {
+    const tilesEnergised = getTilesEnergised(grid, beam)
+    if (tilesEnergised > maxTiles) {
+      maxTiles = tilesEnergised
+    }
+  })
+
+  return maxTiles
+}
 
 module.exports = {
   part1,
--- 2023/16-the-floor-will-be-lava/solution.test.js
+++ 2023/16-the-floor-will-be-lava/solution.test.js
@@ -16,9 +16,18 @@ describe("part1", () => {
   })
 })
 
-xdescribe("part2", () => {
-  test("returns something", () => {
-    const input = `puzzle input`
-    expect(part2(input)).toEqual(0)
+describe("part2", () => {
+  test("returns max number of tiles that can be energised in any configuration", () => {
+    const input = String.raw`.|...\....
+|.-.\.....
+.....|-...
+........|.
+..........
+.........\
+..../.\\..
+.-.-/..|..
+.|....-|.\
+..//.|....`
+    expect(part2(input)).toEqual(51)
   })
 })
