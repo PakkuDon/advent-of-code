3c06544ff93a6d8da5590fe67cd3ba6537fd4daa
PakkuDon <pakku.pd@gmail.com>
2023-12-19 00:27:16 +1100

    Add solution for 2023, Day 18, Part 2

--- 2023/18-lavaduct-lagoon/solution.js
+++ 2023/18-lavaduct-lagoon/solution.js
@@ -108,7 +108,72 @@ const part1 = (input) => {
   return filledTiles
 }
 
-const part2 = (input) => {}
+const part2 = (input) => {
+  const hexToDirection = {
+    0: "R",
+    1: "D",
+    2: "L",
+    3: "U",
+  }
+  const instructions = input
+    .trim()
+    .split("\n")
+    .map((row) => {
+      const segments = row.split(" ")
+      const hexSequence = segments[2].replace(/[#()]/g, "")
+
+      return {
+        direction: hexToDirection[hexSequence.substring(5)],
+        meters: parseInt(hexSequence.substring(0, 5), 16),
+      }
+    })
+
+  const digger = { x: 1, y: 1 }
+
+  // Find vertices and perimeter of route
+  const visited = []
+  visited.push({ x: digger.x, y: digger.y })
+
+  let perimeter = 0
+  instructions.forEach((instruction) => {
+    switch (instruction.direction) {
+      case "R":
+        digger.x += instruction.meters
+        break
+      case "U":
+        digger.y -= instruction.meters
+        break
+      case "D":
+        digger.y += instruction.meters
+        break
+      case "L":
+        digger.x -= instruction.meters
+        break
+    }
+    perimeter += instruction.meters
+    visited.push({ x: digger.x, y: digger.y })
+  })
+
+  // Calculate area of polygon using shoelace formula
+  // https://rosettacode.org/wiki/Shoelace_formula_for_polygonal_area
+  let area = 0
+  for (let i = 0; i < visited.length - 1; i++) {
+    const tile = visited[i]
+    const nextTile = visited[i + 1]
+    area += tile.x * nextTile.y - nextTile.x * tile.y
+  }
+  area =
+    Math.abs(
+      area +
+        perimeter +
+        visited[visited.length - 1].x * visited[0].y -
+        visited[0].x * visited[visited.length - 1].y
+    ) /
+      2 +
+    1
+
+  return area
+}
 
 module.exports = {
   part1,
--- 2023/18-lavaduct-lagoon/solution.test.js
+++ 2023/18-lavaduct-lagoon/solution.test.js
@@ -20,9 +20,22 @@ U 2 (#7a21e3)`
   })
 })
 
-xdescribe("part2", () => {
-  test("returns something", () => {
-    const input = `puzzle input`
-    expect(part2(input)).toEqual(0)
+describe("part2", () => {
+  test("returns how many cubic meters dig plan can hold given hexadecimal instructions", () => {
+    const input = `R 6 (#70c710)
+D 5 (#0dc571)
+L 2 (#5713f0)
+D 2 (#d2c081)
+R 2 (#59c680)
+D 2 (#411b91)
+L 5 (#8ceee2)
+U 2 (#caa173)
+L 1 (#1b58a2)
+U 2 (#caa171)
+R 2 (#7807d2)
+U 3 (#a77fa3)
+L 2 (#015232)
+U 2 (#7a21e3)`
+    expect(part2(input)).toEqual(952408144115)
   })
 })
