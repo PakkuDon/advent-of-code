1c41b0dc5b70a992cd36981b33a9b25801ab57be
PakkuDon <pakku.pd@gmail.com>
2023-12-19 13:46:31 +1100

    Add solution for 2023, Day 15, Part 2

--- 2023/15-lens-library/solution.js
+++ 2023/15-lens-library/solution.js
@@ -1,23 +1,80 @@
+const calculateValue = (sequence) => {
+  let value = 0
+  for (let i = 0; i < sequence.length; i++) {
+    const char = sequence[i]
+    value += char.charCodeAt(0)
+    value *= 17
+    value %= 256
+  }
+
+  return value
+}
+
 const part1 = (input) => {
   const instructions = input.trim().split(",")
   let sum = 0
 
   instructions.forEach((instruction) => {
-    let value = 0
-    for (let i = 0; i < instruction.length; i++) {
-      const char = instruction[i]
-      value += char.charCodeAt(0)
-      value *= 17
-      value %= 256
-    }
-
-    sum += value
+    sum += calculateValue(instruction)
   })
 
   return sum
 }
 
-const part2 = (input) => {}
+const part2 = (input) => {
+  const instructions = input
+    .trim()
+    .split(",")
+    .map((sequence) => {
+      let segments = sequence.split(/\b/)
+      return {
+        label: segments[0],
+        operation: segments[1],
+        focalLength: segments[2],
+      }
+    })
+
+  // Organise lens by instructions
+  const boxes = new Array(256).fill().map(() => [])
+  instructions.forEach((instruction) => {
+    const index = calculateValue(instruction.label)
+    if (instruction.operation === "-") {
+      const box = boxes[index]
+      const indexToDelete = box.findIndex(
+        (lens) => lens.label === instruction.label
+      )
+      if (indexToDelete > -1) {
+        boxes[index] = [
+          ...box.slice(0, indexToDelete),
+          ...box.slice(indexToDelete + 1),
+        ]
+      }
+    } else if (instruction.operation === "=") {
+      const box = boxes[index]
+      const indexOfPreviousInstance = box.findIndex(
+        (lens) => lens.label === instruction.label
+      )
+      if (indexOfPreviousInstance > -1) {
+        boxes[index] = [
+          ...box.slice(0, indexOfPreviousInstance),
+          instruction,
+          ...box.slice(indexOfPreviousInstance + 1),
+        ]
+      } else {
+        box.push(instruction)
+      }
+    }
+  })
+
+  // Calculate focusing power
+  let focusingPower = 0
+  for (let i = 0; i < boxes.length; i++) {
+    boxes[i].forEach((lens, index) => {
+      focusingPower += (i + 1) * (index + 1) * lens.focalLength
+    })
+  }
+  return focusingPower
+}
 
 module.exports = {
   part1,
--- 2023/15-lens-library/solution.test.js
+++ 2023/15-lens-library/solution.test.js
@@ -7,9 +7,9 @@ describe("part1", () => {
   })
 })
 
-xdescribe("part2", () => {
-  test("returns something", () => {
-    const input = `puzzle input`
-    expect(part2(input)).toEqual(0)
+describe("part2", () => {
+  test("returns focusing power given resulting lens configuration", () => {
+    const input = `rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7`
+    expect(part2(input)).toEqual(145)
   })
 })
