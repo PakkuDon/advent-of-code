8726dc2d6d75c24d9c94643dd81064d18b55c0d6
PakkuDon <pakku.pd@gmail.com>
2024-01-05 17:25:14 +1100

    Refactor solution for 2020, Day 5

--- 2020/05-binary-boarding/solution.js
+++ 2020/05-binary-boarding/solution.js
@@ -1,3 +1,14 @@
+const getSeatIds = (boardingPasses) =>
+  boardingPasses.map((pass) => {
+    const rowInput = pass.substring(0, 7)
+    const seatInput = pass.substring(7)
+
+    const row = processBinaryPartition(rowInput, 0, 127)
+    const column = processBinaryPartition(seatInput, 0, 7)
+
+    return row * 8 + column
+  })
+
 const processBinaryPartition = (instructions, lowerBound, upperBound) => {
   const range = [lowerBound, upperBound]
   instructions.split("").forEach((char) => {
@@ -14,29 +25,13 @@ const processBinaryPartition = (instructions, lowerBound, upperBound) => {
 
 const part1 = (input) => {
   const boardingPasses = input.trim().split("\n")
-  const seatIds = boardingPasses.map((pass) => {
-    const rowInput = pass.substring(0, 7)
-    const seatInput = pass.substring(7)
-
-    const row = processBinaryPartition(rowInput, 0, 127)
-    const column = processBinaryPartition(seatInput, 0, 7)
-
-    return row * 8 + column
-  })
+  const seatIds = getSeatIds(boardingPasses)
   return Math.max(...seatIds)
 }
 
 const part2 = (input) => {
   const boardingPasses = input.trim().split("\n")
-  const seatIds = boardingPasses.map((pass) => {
-    const rowInput = pass.substring(0, 7)
-    const seatInput = pass.substring(7)
-
-    const row = processBinaryPartition(rowInput, 0, 127)
-    const column = processBinaryPartition(seatInput, 0, 7)
-
-    return row * 8 + column
-  })
+  const seatIds = getSeatIds(boardingPasses)
   seatIds.sort((a, b) => a - b)
 
   // Find missing seat ID
