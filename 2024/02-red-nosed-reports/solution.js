afcb3206fa93adf278c80f1d12cb444b8df3466f
PakkuDon <pakku.pd@gmail.com>
2024-12-02 16:40:58 +1100

    Refactor solution for 2024, Day 2, Part 1

--- 2024/02-red-nosed-reports/solution.js
+++ 2024/02-red-nosed-reports/solution.js
@@ -1,22 +1,22 @@
-const part1 = (input) => {
-  const reports = input.split("\n").map(value => value.split(/\s/).map(cell => parseInt(cell, 10)))
+const isSafeReport = (report) => {
+  if (report.toSorted((a, b) => a < b ? -1 : 1).join(",") !== report.join(",")
+    && report.toSorted((a, b) => a < b ? 1 : -1).join(",") !== report.join(",")) {
+    return false
+  }
 
-  const safeReports = reports.filter(report => {
-    // Reject reports that are not either all ascending or all descending
-    if (report.toSorted((a, b) => a < b ? -1 : 1).join(",") !== report.join(",")
-      && report.toSorted((a, b) => a < b ? 1 : -1).join(",") !== report.join(",")) {
+  for (let i = 1; i < report.length; i++) {
+    const difference = Math.abs(report[i] - report[i - 1])
+    if (difference < 1 || difference > 3) {
       return false
     }
+  }
 
-    for (let i = 1; i < report.length; i++) {
-      const difference = Math.abs(report[i] - report[i - 1])
-      if (difference < 1 || difference > 3) {
-        return false
-      }
-    }
-    return true
-  })
-  return safeReports.length
+  return true
+}
+
+const part1 = (input) => {
+  const reports = input.split("\n").map(value => value.split(/\s/).map(cell => parseInt(cell, 10)))
+  return reports.filter(isSafeReport).length
 }
 
 const part2 = (input) => {}
