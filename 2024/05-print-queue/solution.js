852d5b9e2b16ec358d883cbf44d5315f235a396b
PakkuDon <pakku.pd@gmail.com>
2024-12-05 16:21:30 +1100

    Add solution for 2024, Day 5, Part 1

---
+++ 2024/05-print-queue/solution.js
@@ -0,0 +1,53 @@
+const part1 = (input) => {
+  const [pageOrderingRulesInput, pageUpdatesInput] = input.trim().split("\n\n")
+  const pageOrderingRules = pageOrderingRulesInput
+    .split("\n")
+    .map((row) => row.split("|").map((value) => parseInt(value, 10)))
+  const pageUpdates = pageUpdatesInput
+    .split("\n")
+    .map((row) => row.split(",").map((value) => parseInt(value, 10)))
+
+  const validPageUpdates = pageUpdates.filter((pageUpdate) => {
+    for (let i = 0; i < pageUpdate.length; i++) {
+      const pageNo = pageUpdate[i]
+
+      // Check that current page is pageUpdate is after all pages specified in rules
+      const afterPage = pageOrderingRules
+        .filter((rule) => pageUpdate.includes(rule[0]) && rule[1] === pageNo)
+        .map((rule) => rule[0])
+      if (
+        !afterPage.every((otherPage) => {
+          return pageUpdate.indexOf(pageNo) > pageUpdate.indexOf(otherPage)
+        })
+      ) {
+        return false
+      }
+
+      // Check that current page is pageUpdate is before all pages specified in rules
+      const beforePage = pageOrderingRules
+        .filter((rule) => pageUpdate.includes(rule[1]) && rule[0] === pageNo)
+        .map((rule) => rule[1])
+      if (
+        !beforePage.every((otherPage) => {
+          return pageUpdate.indexOf(pageNo) < pageUpdate.indexOf(otherPage)
+        })
+      ) {
+        return false
+      }
+    }
+
+    return true
+  })
+
+  return validPageUpdates.reduce((total, pageUpdate) => {
+    const middle = pageUpdate[Math.floor(pageUpdate.length / 2)]
+    return total + middle
+  }, 0)
+}
+
+const part2 = (input) => {}
+
+module.exports = {
+  part1,
+  part2,
+}
