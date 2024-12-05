fc976ab32c4b2ab6d386b134cd11a0e3dd5516bf
PakkuDon <pakku.pd@gmail.com>
2024-12-05 17:10:13 +1100

    Add solution for 2024, Day 5, Part 2

--- 2024/05-print-queue/solution.js
+++ 2024/05-print-queue/solution.js
@@ -48,7 +48,44 @@ const part1 = (input) => {
   }, 0)
 }
 
-const part2 = (input) => {}
+const part2 = (input) => {
+  const [pageOrderingRulesInput, pageUpdatesInput] = input.trim().split("\n\n")
+  const pageOrderingRules = pageOrderingRulesInput
+    .split("\n")
+    .map((row) => row.split("|").map((value) => parseInt(value, 10)))
+  const pageUpdates = pageUpdatesInput
+    .split("\n")
+    .map((row) => row.split(",").map((value) => parseInt(value, 10)))
+
+  const invalidPageUpdates = pageUpdates.filter(
+    (pageUpdate) => !isValidPageUpdate(pageUpdate, pageOrderingRules)
+  )
+
+  // Map out which pages have to come before other pages
+  const beforePages = {}
+  pageOrderingRules.forEach((rule) => {
+    if (!beforePages[rule[0]]) {
+      beforePages[rule[0]] = []
+    }
+    beforePages[rule[0]].push(rule[1])
+  })
+
+  // Sort page updates by page rules
+  const fixedPageUpdates = invalidPageUpdates.map((pageUpdate) => {
+    return pageUpdate.toSorted((a, b) => {
+      if (beforePages[a] && beforePages[a].includes(b)) {
+        return -1
+      } else {
+        return 1
+      }
+    })
+  })
+
+  return fixedPageUpdates.reduce((total, pageUpdate) => {
+    const middle = pageUpdate[Math.floor(pageUpdate.length / 2)]
+    return total + middle
+  }, 0)
+}
 
 module.exports = {
   part1,
