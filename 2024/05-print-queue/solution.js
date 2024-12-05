05c51c478bf83fde53d54a07d4c254e1fc1b6f22
PakkuDon <pakku.pd@gmail.com>
2024-12-05 16:27:34 +1100

    Refactor solution for 2024, Day 5, Part 1

--- 2024/05-print-queue/solution.js
+++ 2024/05-print-queue/solution.js
@@ -1,3 +1,34 @@
+const isValidPageUpdate = (pageUpdate, pageOrderingRules) => {
+  for (let i = 0; i < pageUpdate.length; i++) {
+    const pageNo = pageUpdate[i]
+
+    // Check that current page is pageUpdate is after all pages specified in rules
+    const afterPage = pageOrderingRules
+      .filter((rule) => pageUpdate.includes(rule[0]) && rule[1] === pageNo)
+      .map((rule) => rule[0])
+    if (
+      !afterPage.every((otherPage) => {
+        return pageUpdate.indexOf(pageNo) > pageUpdate.indexOf(otherPage)
+      })
+    ) {
+      return false
+    }
+
+    // Check that current page is pageUpdate is before all pages specified in rules
+    const beforePage = pageOrderingRules
+      .filter((rule) => pageUpdate.includes(rule[1]) && rule[0] === pageNo)
+      .map((rule) => rule[1])
+    if (
+      !beforePage.every((otherPage) => {
+        return pageUpdate.indexOf(pageNo) < pageUpdate.indexOf(otherPage)
+      })
+    ) {
+      return false
+    }
+  }
+  return true
+}
+
 const part1 = (input) => {
   const [pageOrderingRulesInput, pageUpdatesInput] = input.trim().split("\n\n")
   const pageOrderingRules = pageOrderingRulesInput
@@ -7,37 +38,9 @@ const part1 = (input) => {
     .split("\n")
     .map((row) => row.split(",").map((value) => parseInt(value, 10)))
 
-  const validPageUpdates = pageUpdates.filter((pageUpdate) => {
-    for (let i = 0; i < pageUpdate.length; i++) {
-      const pageNo = pageUpdate[i]
-
-      // Check that current page is pageUpdate is after all pages specified in rules
-      const afterPage = pageOrderingRules
-        .filter((rule) => pageUpdate.includes(rule[0]) && rule[1] === pageNo)
-        .map((rule) => rule[0])
-      if (
-        !afterPage.every((otherPage) => {
-          return pageUpdate.indexOf(pageNo) > pageUpdate.indexOf(otherPage)
-        })
-      ) {
-        return false
-      }
-
-      // Check that current page is pageUpdate is before all pages specified in rules
-      const beforePage = pageOrderingRules
-        .filter((rule) => pageUpdate.includes(rule[1]) && rule[0] === pageNo)
-        .map((rule) => rule[1])
-      if (
-        !beforePage.every((otherPage) => {
-          return pageUpdate.indexOf(pageNo) < pageUpdate.indexOf(otherPage)
-        })
-      ) {
-        return false
-      }
-    }
-
-    return true
-  })
+  const validPageUpdates = pageUpdates.filter((pageUpdate) =>
+    isValidPageUpdate(pageUpdate, pageOrderingRules)
+  )
 
   return validPageUpdates.reduce((total, pageUpdate) => {
     const middle = pageUpdate[Math.floor(pageUpdate.length / 2)]
