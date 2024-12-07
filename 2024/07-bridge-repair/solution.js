a82f7d01c0cce3953cad45ed9232ad8b9535d052
PakkuDon <pakku.pd@gmail.com>
2024-12-07 17:12:01 +1100

    Add solution for 2024, Day 7, Part 1

---
+++ 2024/07-bridge-repair/solution.js
@@ -0,0 +1,43 @@
+const part1 = (input) => {
+  // Parse input
+  const equations = input
+    .trim()
+    .split("\n")
+    .map((row) => row.split(/:? /).map((value) => Number(value)))
+
+  // Determine which equations can match left-most value
+  const validEquations = equations.filter((equation) => {
+    const [target, ...numbers] = equation
+
+    for (let i = 0; i < Math.pow(2, numbers.length); i++) {
+      const mask = i
+        .toString(2)
+        .padStart(numbers.length - 1, "0")
+        .replace(/0/g, "+")
+        .replace(/1/g, "*")
+
+      let result = numbers[0]
+      for (let j = 1; j < numbers.length; j++) {
+        if (mask[j] === "+") {
+          result += numbers[j]
+        } else {
+          result *= numbers[j]
+        }
+      }
+
+      if (result === target) {
+        return true
+      }
+    }
+  })
+
+  // Return sum of valid equations
+  return validEquations.reduce((total, equation) => total + equation[0], 0)
+}
+
+const part2 = (input) => {}
+
+module.exports = {
+  part1,
+  part2,
+}
---
+++ 2024/07-bridge-repair/solution.test.js
@@ -0,0 +1,23 @@
+const { part1, part2 } = require("./solution")
+
+describe("part1", () => {
+  test("returns total calibration result", () => {
+    const input = `190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20`
+    expect(part1(input)).toEqual(3749)
+  })
+})
+
+xdescribe("part2", () => {
+  test("returns something", () => {
+    const input = `puzzle input`
+    expect(part2(input)).toEqual(0)
+  })
+})
