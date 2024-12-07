5e180395ae971fda911149e870de39a2d3555f24
PakkuDon <pakku.pd@gmail.com>
2024-12-07 17:19:50 +1100

    Add solution for 2024, Day 7, Part 2

--- 2024/07-bridge-repair/solution.js
+++ 2024/07-bridge-repair/solution.js
@@ -35,7 +35,50 @@ const part1 = (input) => {
   return validEquations.reduce((total, equation) => total + equation[0], 0)
 }
 
-const part2 = (input) => {}
+const part2 = (input) => {
+  // Parse input
+  const equations = input
+    .trim()
+    .split("\n")
+    .map((row) => row.split(/:? /).map((value) => Number(value)))
+
+  // Determine which equations can match left-most value
+  const validEquations = equations.filter((equation) => {
+    const [target, ...numbers] = equation
+
+    for (let i = 0; i < Math.pow(3, numbers.length); i++) {
+      const mask = i
+        .toString(3)
+        .padStart(numbers.length - 1, "0")
+        .replace(/0/g, "+")
+        .replace(/1/g, "*")
+        .replace(/2/g, "|")
+
+      let result = numbers[0]
+      for (let j = 1; j < numbers.length; j++) {
+        if (mask[j] === "+") {
+          result += numbers[j]
+        } else if (mask[j] === "*") {
+          result *= numbers[j]
+        } else {
+          result = Number(result + "" + numbers[j])
+        }
+
+        // Break early if potential result of current mask exceeds target
+        if (result > target) {
+          break
+        }
+      }
+
+      if (result === target) {
+        return true
+      }
+    }
+  })
+
+  // Return sum of valid equations
+  return validEquations.reduce((total, equation) => total + equation[0], 0)
+}
 
 module.exports = {
   part1,
--- 2024/07-bridge-repair/solution.test.js
+++ 2024/07-bridge-repair/solution.test.js
@@ -15,9 +15,17 @@ describe("part1", () => {
   })
 })
 
-xdescribe("part2", () => {
-  test("returns something", () => {
-    const input = `puzzle input`
-    expect(part2(input)).toEqual(0)
+describe("part2", () => {
+  test("returns total calibration result account for concatenator operator", () => {
+    const input = `190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20`
+    expect(part2(input)).toEqual(11387)
   })
 })
