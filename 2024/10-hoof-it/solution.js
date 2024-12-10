6b7353e3137e80ecb1cffe8f25e3a9e159b41d83
PakkuDon <pakku.pd@gmail.com>
2024-12-10 17:04:08 +1100

    Add solution for 2024, Day 10, Part 2

--- 2024/10-hoof-it/solution.js
+++ 2024/10-hoof-it/solution.js
@@ -69,7 +69,70 @@ const part1 = (input) => {
   return scores.reduce((total, current) => total + current, 0)
 }
 
-const part2 = (input) => {}
+const part2 = (input) => {
+  // Parse input
+  const grid = input
+    .trim()
+    .split("\n")
+    .map((row) => row.split("").map((value) => Number(value)))
+  const height = grid.length
+  const width = grid[0].length
+
+  // Find trailheads
+  const trailheads = []
+  for (let y = 0; y < height; y++) {
+    for (let x = 0; x < width; x++) {
+      if (grid[y][x] === 0) {
+        trailheads.push({ x, y })
+      }
+    }
+  }
+
+  // Calculate trailhead scores
+  const scores = []
+  trailheads.forEach((trailhead) => {
+    const stack = []
+    stack.push(trailhead)
+
+    let current
+    let score = 0
+
+    // DFS to find which tiles with height 9 trailhead can reach
+    while (stack.length > 0) {
+      current = stack.pop()
+      if (grid[current.y][current.x] === 9) {
+        score++
+      }
+
+      // Push available neighbours onto stack
+      if (current.x - 1 >= 0) {
+        if (grid[current.y][current.x - 1] === grid[current.y][current.x] + 1) {
+          stack.push({ x: current.x - 1, y: current.y })
+        }
+      }
+      if (current.x + 1 < width) {
+        if (grid[current.y][current.x + 1] === grid[current.y][current.x] + 1) {
+          stack.push({ x: current.x + 1, y: current.y })
+        }
+      }
+      if (current.y - 1 >= 0) {
+        if (grid[current.y - 1][current.x] === grid[current.y][current.x] + 1) {
+          stack.push({ x: current.x, y: current.y - 1 })
+        }
+      }
+      if (current.y + 1 < height) {
+        if (grid[current.y + 1][current.x] === grid[current.y][current.x] + 1) {
+          stack.push({ x: current.x, y: current.y + 1 })
+        }
+      }
+    }
+
+    scores.push(score)
+  })
+
+  // Sum trailhead scores
+  return scores.reduce((total, current) => total + current, 0)
+}
 
 module.exports = {
   part1,
--- 2024/10-hoof-it/solution.test.js
+++ 2024/10-hoof-it/solution.test.js
@@ -14,9 +14,16 @@ describe("part1", () => {
   })
 })
 
-xdescribe("part2", () => {
-  test("returns something", () => {
-    const input = `puzzle input`
-    expect(part2(input)).toEqual(0)
+describe("part2", () => {
+  test("returns sum of all trailhead ratings", () => {
+    const input = `89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732`
+    expect(part2(input)).toEqual(81)
   })
 })
