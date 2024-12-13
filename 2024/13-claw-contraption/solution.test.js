e201f31d6801eca7831ef44f2f6c153b5e27645f
PakkuDon <pakku.pd@gmail.com>
2024-12-13 16:33:49 +1100

    Add solution for 2024, Day 13, Part 1

---
+++ 2024/13-claw-contraption/solution.test.js
@@ -0,0 +1,29 @@
+const { part1, part2 } = require("./solution")
+
+describe("part1", () => {
+  test("returns minimum number of tokens to win all prizes", () => {
+    const input = `Button A: X+94, Y+34
+Button B: X+22, Y+67
+Prize: X=8400, Y=5400
+
+Button A: X+26, Y+66
+Button B: X+67, Y+21
+Prize: X=12748, Y=12176
+
+Button A: X+17, Y+86
+Button B: X+84, Y+37
+Prize: X=7870, Y=6450
+
+Button A: X+69, Y+23
+Button B: X+27, Y+71
+Prize: X=18641, Y=10279`
+    expect(part1(input)).toEqual(480)
+  })
+})
+
+xdescribe("part2", () => {
+  test("returns something", () => {
+    const input = `puzzle input`
+    expect(part2(input)).toEqual(0)
+  })
+})
