f7782ee5d32414592ff2be4d6c1bc9e678173f76
PakkuDon <pakku.pd@gmail.com>
2024-12-17 14:39:17 +1100

    Add solution for 2024, Day 16, Part 2

--- 2024/16-reindeer-maze/solution.js
+++ 2024/16-reindeer-maze/solution.js
@@ -106,7 +106,129 @@ const part1 = (input) => {
   return getLowestScore(grid, start, end)
 }
 
-const part2 = (input) => {}
+const part2 = (input) => {
+  // Parse input
+  const grid = input
+    .trim()
+    .split("\n")
+    .map((row) => row.split(""))
+
+  // Find start and end locations
+  let start, end
+  for (let y = 0; y < grid.length; y++) {
+    for (let x = 0; x < grid[y].length; x++) {
+      if (grid[y][x] === "S") {
+        start = { x, y }
+      } else if (grid[y][x] === "E") {
+        end = { x, y }
+      }
+    }
+  }
+
+  // Find shortest path again, but this time track possible paths with best score
+  const targetCost = getLowestScore(grid, start, end)
+  const costs = {}
+  const queue = []
+  const paths = []
+  queue.push({ path: [{ ...start, direction: "E" }], cost: 0 })
+
+  while (queue.length > 0) {
+    const { path, cost } = queue.shift()
+
+    // Skip paths that cost more than shortest path
+    if (cost > targetCost) {
+      continue
+    }
+
+    const current = path[path.length - 1]
+
+    if (current.x === end.x && current.y === end.y && cost === targetCost) {
+      paths.push(path)
+      continue
+    }
+
+    // Record paths that lead to solution
+    if (
+      costs[`${current.x},${current.y},${current.direction}`] &&
+      costs[`${current.x},${current.y},${current.direction}`] < cost
+    ) {
+      continue
+    }
+    costs[`${current.x},${current.y},${current.direction}`] = cost
+
+    // Add potential neighbours to stack to explore
+    // Up
+    if (grid[current.y - 1][current.x] !== "#") {
+      const currentCost = cost + (current.direction === "N" ? 1 : 1001)
+      queue.push({
+        path: [
+          ...path,
+          {
+            x: current.x,
+            y: current.y - 1,
+            direction: "N",
+          },
+        ],
+        cost: currentCost,
+      })
+    }
+    // Right
+    if (grid[current.y][current.x + 1] !== "#") {
+      const currentCost = cost + (current.direction === "E" ? 1 : 1001)
+      queue.push({
+        path: [
+          ...path,
+          {
+            x: current.x + 1,
+            y: current.y,
+            direction: "E",
+          },
+        ],
+        cost: currentCost,
+      })
+    }
+    // Down
+    if (grid[current.y + 1][current.x] !== "#") {
+      const currentCost = cost + (current.direction === "S" ? 1 : 1001)
+      queue.push({
+        path: [
+          ...path,
+          {
+            x: current.x,
+            y: current.y + 1,
+            direction: "S",
+          },
+        ],
+        cost: currentCost,
+      })
+    }
+    // Left
+    if (grid[current.y][current.x - 1] !== "#") {
+      const currentCost = cost + (current.direction === "W" ? 1 : 1001)
+      queue.push({
+        path: [
+          ...path,
+          {
+            x: current.x - 1,
+            y: current.y,
+            direction: "W",
+          },
+        ],
+        cost: currentCost,
+      })
+    }
+  }
+
+  // Count available seats on best paths
+  const tiles = new Set()
+  paths.forEach((path) => {
+    path.forEach(({ x, y }) => {
+      tiles.add(`${x},${y}`)
+    })
+  })
+
+  return tiles.size
+}
 
 module.exports = {
   part1,
