ae51e1fc02f9e03ed76c9237f87556c74e719587
PakkuDon <pakku.pd@gmail.com>
2025-01-05 00:30:05 +1100

    Add solution for 2015, Day 14, Part 2

--- 2015/14-reindeer-olympics/solution.js
+++ 2015/14-reindeer-olympics/solution.js
@@ -47,7 +47,74 @@ const part1 = (input, raceDuration) => {
   return Math.max(...distances)
 }
 
-const part2 = (input) => {}
+const part2 = (input, raceDuration) => {
+  // Parse input
+  const reindeers = input
+    .trim()
+    .split("\n")
+    .map((row) => {
+      const [speed, duration, restTime] = row
+        .match(/\d+/g)
+        .map((value) => Number(value))
+      return { speed, duration, restTime }
+    })
+
+  // Calculate distances travelled by each reindeer each second
+  const distances = reindeers.map((reindeer) => {
+    let distancesPerSecond = []
+    let distance = 0
+    let secondsEllapsed = 0
+    let secondsTravelled = 0
+    let secondsRested = 0
+    let isResting = false
+
+    while (secondsEllapsed < raceDuration) {
+      // Flip state if reindeer has travelled or rested for allowed amount
+      if (secondsTravelled === reindeer.duration) {
+        isResting = true
+        secondsTravelled = 0
+        secondsRested = 0
+      } else if (secondsRested === reindeer.restTime) {
+        isResting = false
+        secondsTravelled = 0
+        secondsRested = 0
+      }
+
+      if (isResting) {
+        secondsRested++
+      } else {
+        distance += reindeer.speed
+        secondsTravelled++
+      }
+
+      distancesPerSecond.push(distance)
+      secondsEllapsed++
+    }
+
+    return distancesPerSecond
+  })
+
+  // Count points awarded to each reindeer
+  const points = reindeers.map(() => 0)
+  for (let i = 0; i < distances[0].length; i++) {
+    // Get distances travelled in current second
+    const currentDistances = []
+    for (let j = 0; j < reindeers.length; j++) {
+      currentDistances.push(distances[j][i])
+    }
+
+    // Award points to reindeers in lead at given second
+    const maxDistance = Math.max(...currentDistances)
+    for (let j = 0; j < reindeers.length; j++) {
+      if (currentDistances[j] === maxDistance) {
+        points[j]++
+      }
+    }
+  }
+
+  // Return points gathered by winning reindeer
+  return Math.max(...points)
+}
 
 module.exports = {
   part1,
--- 2015/14-reindeer-olympics/solution.test.js
+++ 2015/14-reindeer-olympics/solution.test.js
@@ -8,9 +8,10 @@ Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.`
   })
 })
 
-xdescribe("part2", () => {
-  test("returns something", () => {
-    const input = `puzzle input`
-    expect(part2(input)).toEqual(0)
+describe("part2", () => {
+  test("returns points gathered by winning reindeer", () => {
+    const input = `Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.
+Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.`
+    expect(part2(input, 1000)).toEqual(689)
   })
 })
