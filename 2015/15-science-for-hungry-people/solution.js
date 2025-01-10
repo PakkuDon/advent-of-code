94e456b0c921f3e5358f441c64014a278933a5af
PakkuDon <pakku.pd@gmail.com>
2025-01-10 17:42:12 +1100

    Add solution for 2015, Day 15, Part 1

---
+++ 2015/15-science-for-hungry-people/solution.js
@@ -0,0 +1,85 @@
+const parseIngredients = (input) => {
+  return input
+    .trim()
+    .split("\n")
+    .map((row) => {
+      const properties = row.split(": ")[1]
+      const values = properties.match(/-?\d+/g).map((value) => Number(value))
+
+      return {
+        capacity: values[0],
+        durability: values[1],
+        flavor: values[2],
+        texture: values[3],
+        calories: values[4],
+      }
+    })
+}
+
+const part1 = (input) => {
+  const ingredients = parseIngredients(input)
+
+  // Check possible combinations of ingredients that can yield highest score
+  let maxScore = Number.MIN_SAFE_INTEGER
+  // n-1 number of nested loops where n = number of ingredients
+  // Test input uses 2 ingredients. Real input uses 4
+  if (ingredients.length === 2) {
+    for (let i = 0; i < 100; i++) {
+      const j = 100 - i
+      const scores = []
+      for (let key in ingredients[0]) {
+        if (key !== "calories") {
+          scores.push(i * ingredients[0][key] + j * ingredients[1][key])
+        }
+      }
+
+      const score = scores.reduce(
+        (total, score) => (score > 0 ? total * score : 0),
+        1
+      )
+      if (score > maxScore) {
+        maxScore = score
+      }
+    }
+  } else {
+    for (let i = 0; i < 100; i++) {
+      for (let j = 0; j < 100; j++) {
+        for (let k = 0; k < 100; k++) {
+          const l = 100 - i - j - k
+          if (l < 0) {
+            continue
+          }
+
+          const scores = []
+          for (let key in ingredients[0]) {
+            if (key !== "calories") {
+              scores.push(
+                i * ingredients[0][key] +
+                  j * ingredients[1][key] +
+                  k * ingredients[2][key] +
+                  l * ingredients[3][key]
+              )
+            }
+          }
+
+          const score = scores.reduce(
+            (total, score) => (score > 0 ? total * score : 0),
+            1
+          )
+          if (score > maxScore) {
+            maxScore = score
+          }
+        }
+      }
+    }
+  }
+
+  return maxScore
+}
+
+const part2 = (input) => {}
+
+module.exports = {
+  part1,
+  part2,
+}
---
+++ 2015/15-science-for-hungry-people/solution.test.js
@@ -0,0 +1,16 @@
+const { part1, part2 } = require("./solution")
+
+describe("part1", () => {
+  test("returns score of highest-scoring cookie", () => {
+    const input = `Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8
+Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3`
+    expect(part1(input)).toEqual(62842880)
+  })
+})
+
+xdescribe("part2", () => {
+  test("returns something", () => {
+    const input = `puzzle input`
+    expect(part2(input)).toEqual(0)
+  })
+})
