7eb19df856f80c310e9be4a27394ac9301b8cfd8
PakkuDon <pakku.pd@gmail.com>
2025-01-11 15:34:16 +1100

    Add solution for 2015, Day 16, Part 2

--- 2015/16-aunt-sue/solution.js
+++ 2015/16-aunt-sue/solution.js
@@ -40,7 +40,38 @@ const part1 = (input) => {
   return index + 1
 }
 
-const part2 = (input) => {}
+const part2 = (input) => {
+  // Message from puzzle description
+  const tickerTape = {
+    children: 3,
+    cats: 7,
+    samoyeds: 2,
+    pomeranians: 3,
+    akitas: 0,
+    vizslas: 0,
+    goldfish: 5,
+    trees: 3,
+    cars: 2,
+    perfumes: 1,
+  }
+
+  const aunts = parseInput(input)
+
+  // Find aunt with gift matching ranges or amounts in ticker tape
+  const index = aunts.findIndex((aunt) =>
+    Object.keys(aunt).every((item) => {
+      if (item === "cats" || item === "trees") {
+        return aunt[item] > tickerTape[item]
+      }
+      if (item === "pomeranians" || item === "goldfish") {
+        return aunt[item] < tickerTape[item]
+      }
+      return aunt[item] === tickerTape[item]
+    })
+  )
+  // +1 as aunt input starts from 1
+  return index + 1
+}
 
 module.exports = {
   part1,
