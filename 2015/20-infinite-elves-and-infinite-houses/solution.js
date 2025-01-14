4afadc11ef8a807701b6f3a42f8d7985b0fb3eff
PakkuDon <pakku.pd@gmail.com>
2025-01-14 22:37:01 +1100

    Add solution for 2015, Day 20, Part 2

--- 2015/20-infinite-elves-and-infinite-houses/solution.js
+++ 2015/20-infinite-elves-and-infinite-houses/solution.js
@@ -19,7 +19,26 @@ const part1 = (input) => {
   return houseNo
 }
 
-const part2 = (input) => {}
+const part2 = (input) => {
+  const requiredPresents = Number(input)
+  let houseNo = 1
+
+  while (true) {
+    let presents = 0
+    for (let i = 1; i <= houseNo; i++) {
+      if (houseNo % i === 0 && houseNo <= i * 50) {
+        presents += i * 11
+      }
+    }
+
+    if (presents >= requiredPresents) {
+      break
+    }
+    houseNo++
+  }
+
+  return houseNo
+}
 
 module.exports = {
   part1,
--- 2015/20-infinite-elves-and-infinite-houses/solution.test.js
+++ 2015/20-infinite-elves-and-infinite-houses/solution.test.js
@@ -20,9 +20,6 @@ describe("part1", () => {
   })
 })
 
-xdescribe("part2", () => {
-  test("returns something", () => {
-    const input = `puzzle input`
-    expect(part2(input)).toEqual(0)
-  })
+describe("part2", () => {
+  xtest("no example output provided", () => {})
 })
