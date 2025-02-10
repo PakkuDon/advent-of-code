59f2ae17cc57c0d5dbdaf901b728a0af69b20cee
PakkuDon <pakku.pd@gmail.com>
2025-02-10 23:42:34 +1100

    Add solution for 2016, Day 9, Part 2

--- 2016/09-explosives-in-cyberspace/solution.js
+++ 2016/09-explosives-in-cyberspace/solution.js
@@ -30,7 +30,43 @@ const part1 = (input) => {
   return decompressed.length
 }
 
-const part2 = (input) => {}
+// Return number of characters in decompressed string
+// Recurse to expand subsequences
+const getRecursivelyDecompressedLength = (source) => {
+  let length = 0
+
+  for (let i = 0; i < source.length; i++) {
+    if (source[i] === "(") {
+      // Find closing bracket - Basis for next index to inspect from
+      const endMarkerIndex = source.indexOf(")", i)
+
+      // Parse marker
+      const marker = source.substring(i + 1, endMarkerIndex)
+      const [selectionLength, repeatCount] = marker
+        .match(/\d+/g)
+        .map((value) => Number(value))
+      const substring = source.substring(
+        endMarkerIndex + 1,
+        endMarkerIndex + 1 + selectionLength
+      )
+
+      // Calculcate decompressed length of subsequence
+      length += getRecursivelyDecompressedLength(substring) * repeatCount
+
+      // Update index to skip past marker and decompressed substring
+      i += marker.length + selectionLength + 1
+    } else {
+      // Otherwise append current char to result
+      length++
+    }
+  }
+
+  return length
+}
+
+const part2 = (input) => {
+  return getRecursivelyDecompressedLength(input.trim())
+}
 
 module.exports = {
   part1,
--- 2016/09-explosives-in-cyberspace/solution.test.js
+++ 2016/09-explosives-in-cyberspace/solution.test.js
@@ -35,9 +35,29 @@ describe("part1", () => {
   })
 })
 
-xdescribe("part2", () => {
-  test("returns something", () => {
-    const input = `puzzle input`
-    expect(part2(input)).toEqual(0)
+describe("part2", () => {
+  const testCases = [
+    {
+      input: "(3x3)XYZ",
+      expected: 9,
+    },
+    {
+      input: "X(8x2)(3x3)ABCY",
+      expected: 20,
+    },
+    {
+      input: "(27x12)(20x12)(13x14)(7x10)(1x12)A",
+      expected: 241920,
+    },
+    {
+      input: "(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN",
+      expected: 445,
+    },
+  ]
+
+  testCases.forEach(({ input, expected }) => {
+    test(`example ${input} - returns recursively decompressed length`, () => {
+      expect(part2(input)).toEqual(expected)
+    })
   })
 })
