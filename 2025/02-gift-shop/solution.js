3ac5683110e37f68e25e951c7aea376825b1c64b
PakkuDon <pakku.pd@gmail.com>
2025-12-02 16:26:51 +1100

    Add solution for 2025, Day 2, Part 2

--- 2025/02-gift-shop/solution.js
+++ 2025/02-gift-shop/solution.js
@@ -26,7 +26,30 @@ const part1 = (input) => {
   return invalidIds.reduce((total, current) => total + current, 0)
 }
 
-const part2 = (input) => {}
+const part2 = (input) => {
+  const ranges = input
+    .trim()
+    .split(",")
+    .map((range) => range.split("-").map((value) => Number(value)))
+  const invalidIds = []
+
+  // Find invalid IDs - IDs that are only made up of repeating sequence
+  ranges.forEach(([min, max]) => {
+    for (let i = min; i <= max; i++) {
+      const id = `${i}`
+
+      for (let windowLength = 0; windowLength < id.length - 1; windowLength++) {
+        const substring = id.substring(0, windowLength + 1)
+        const parts = id.split(substring)
+        if (parts.length >= 2 && parts.every((part) => part === "")) {
+          invalidIds.push(Number(id))
+        }
+      }
+    }
+  })
+
+  return [...new Set(invalidIds)].reduce((total, current) => total + current, 0)
+}
 
 module.exports = {
   part1,
--- 2025/02-gift-shop/solution.test.js
+++ 2025/02-gift-shop/solution.test.js
@@ -7,9 +7,9 @@ describe("part1", () => {
   })
 })
 
-xdescribe("part2", () => {
-  test("returns something", () => {
-    const input = `puzzle input`
-    expect(part2(input)).toEqual(0)
+describe("part2", () => {
+  test("returns sum of invalid IDs using new rules", () => {
+    const input = `11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124`
+    expect(part2(input)).toEqual(4174379265)
   })
 })
