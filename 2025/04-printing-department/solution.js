c1e2f4437df27a5492597b7ec189c4034ac8f16d
PakkuDon <pakku.pd@gmail.com>
2025-12-04 16:15:58 +1100

    Add solution for 2025, Day 4, Part 2

--- 2025/04-printing-department/solution.js
+++ 2025/04-printing-department/solution.js
@@ -1,3 +1,54 @@
+const getRollsToRemove = (grid) => {
+  const coordinates = []
+
+  for (let y = 0; y < grid.length; y++) {
+    for (let x = 0; x < grid[y].length; x++) {
+      if (grid[y][x] === "@") {
+        const neighbours = []
+        // Add neighbours
+        // Top-left
+        if (y > 0 && x > 0) {
+          neighbours.push(grid[y - 1][x - 1])
+        }
+        // Top
+        if (y > 0) {
+          neighbours.push(grid[y - 1][x])
+        }
+        // Top-right
+        if (y > 0 && x < grid[y].length - 1) {
+          neighbours.push(grid[y - 1][x + 1])
+        }
+        // Right
+        if (x < grid[y].length - 1) {
+          neighbours.push(grid[y][x + 1])
+        }
+        // Bottom-right
+        if (y < grid.length - 1 && x < grid[y].length - 1) {
+          neighbours.push(grid[y + 1][x + 1])
+        }
+        // Bottom
+        if (y < grid.length - 1) {
+          neighbours.push(grid[y + 1][x])
+        }
+        // Bottom-left
+        if (y < grid.length - 1 && x > 0) {
+          neighbours.push(grid[y + 1][x - 1])
+        }
+        // Left
+        if (x > 0) {
+          neighbours.push(grid[y][x - 1])
+        }
+
+        if (neighbours.filter((cell) => cell === "@").length < 4) {
+          coordinates.push({ x, y })
+        }
+      }
+    }
+  }
+
+  return coordinates
+}
+
 const part1 = (input) => {
   const grid = input.trim().split("\n")
   let count = 0
@@ -50,7 +101,26 @@ const part1 = (input) => {
   return count
 }
 
-const part2 = (input) => {}
+const part2 = (input) => {
+  const grid = input
+    .trim()
+    .split("\n")
+    .map((row) => row.split(""))
+  let totalRollsRemoved = 0
+  let rollsToRemove = getRollsToRemove(grid)
+
+  while (rollsToRemove.length > 0) {
+    // Remove rolls from grid
+    rollsToRemove.forEach(({ x, y }) => {
+      grid[y][x] = "."
+    })
+
+    totalRollsRemoved += rollsToRemove.length
+    rollsToRemove = getRollsToRemove(grid)
+  }
+
+  return totalRollsRemoved
+}
 
 module.exports = {
   part1,
