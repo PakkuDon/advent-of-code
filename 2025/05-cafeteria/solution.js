8e6eac243470b31a7e47ddc2bfb80ad76dd0c3d1
PakkuDon <pakku.pd@gmail.com>
2025-12-05 16:36:32 +1100

    Add solution for 2025, Day 5, Part 2

--- 2025/05-cafeteria/solution.js
+++ 2025/05-cafeteria/solution.js
@@ -28,7 +28,39 @@ const part1 = (input) => {
   return freshIngredientIds.length
 }
 
-const part2 = (input) => {}
+const part2 = (input) => {
+  // Parse input
+  const [rangeIdsInput] = input.trim().split("\n\n")
+  const ranges = rangeIdsInput
+    .split("\n")
+    .map((row) => row.split("-").map((value) => Number(value)))
+
+  // Merge overlapping ranges
+  const sortedRanges = ranges.toSorted(([minA], [minB]) => minA - minB)
+  const mergedRanges = []
+  mergedRanges.push(sortedRanges[0])
+
+  for (let i = 1; i < sortedRanges.length; i++) {
+    const previous = mergedRanges[mergedRanges.length - 1]
+    const current = sortedRanges[i]
+
+    // If current range does not overlap with previous range
+    // add current range as new range
+    if (current[0] > previous[1]) {
+      mergedRanges.push(current)
+    }
+    // Else merge range with previous range
+    else {
+      mergedRanges.pop()
+      mergedRanges.push([previous[0], Math.max(previous[1], current[1])])
+    }
+  }
+
+  return mergedRanges.reduce(
+    (total, range) => total + range[1] - range[0] + 1,
+    0
+  )
+}
 
 module.exports = {
   part1,
